3b1109ea6edfbcd89081e634ec78ab5bbd4cfba2
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-04-16 12:51:35 +0700

    catch exception

--- src/observers/transaction-watcher.observer.ts
+++ src/observers/transaction-watcher.observer.ts
@@ -32,36 +32,40 @@ export class TransactionWatcherObserver implements LifeCycleObserver {
    */
   async start(): Promise<void> {
     // Add your logic for start
-    const wsProvider = new WsProvider('wss://rpc.myriad.systems')
-    const api = await ApiPromise.create({provider: wsProvider})
-    await api.isReady
-    console.log('RPC isReady for TransactionWatcher');
+    try {
+      const wsProvider = new WsProvider('wss://rpc.myriad.systems')
+      const api = await ApiPromise.create({provider: wsProvider})
+      await api.isReady
+      console.log('RPC isReady for TransactionWatcher');
 
-    // Subscribe to system events via storage
-    api.query.system.events((events) => {
-      // Loop through the Vec<EventRecord>
-      events.forEach((record) => {
-        // Extract the phase, event and the event types
-        const {event} = record;
+      // Subscribe to system events via storage
+      api.query.system.events((events) => {
+        // Loop through the Vec<EventRecord>
+        events.forEach((record) => {
+          // Extract the phase, event and the event types
+          const {event} = record;
 
-        // Show what we are busy with
-        if (event.section == 'balances' && event.method == 'Transfer') {
-          const hash = event.hash.toString()
-          const from = event.data[0].toString();
-          const to = event.data[1].toString();
-          const value = event.data[2].toString();
+          // Show what we are busy with
+          if (event.section == 'balances' && event.method == 'Transfer') {
+            const hash = event.hash.toString()
+            const from = event.data[0].toString();
+            const to = event.data[1].toString();
+            const value = event.data[2].toString();
 
-          this.transactionRepository.create({
-            trxHash: hash,
-            from: from,
-            to: to,
-            value: parseInt(value),
-            state: 'success',
-            createdAt: new Date().toString()
-          })
-        }
+            this.transactionRepository.create({
+              trxHash: hash,
+              from: from,
+              to: to,
+              value: parseInt(value),
+              state: 'success',
+              createdAt: new Date().toString()
+            })
+          }
+        })
       })
-    })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   /**
