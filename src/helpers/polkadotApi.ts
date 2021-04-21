2c5f6cbfc45a39c28de492d5a3e4f6df6b4162de
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-04-21 15:20:37 +0700

    catch error websocket

--- src/helpers/polkadotApi.ts
+++ src/helpers/polkadotApi.ts
@@ -2,7 +2,7 @@ import {ApiPromise, WsProvider} from '@polkadot/api';
 
 export async function polkadotApi(): Promise<ApiPromise> {
   const wsProvider = new WsProvider('wss://rpc.myriad.systems')
-  const api = await ApiPromise.create({provider: wsProvider})
+  const api = await new ApiPromise({provider: wsProvider}).isReadyOrError
 
   return api
 }
