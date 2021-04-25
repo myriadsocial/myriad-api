d798e8e62c39f8ce87111633ca1eefb48c01acdb
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-04-25 11:49:35 +0700

    use port 3000

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ if (require.main === module) {
   // Run the application
   const config = {
     rest: {
-      port: +(process.env.PORT ?? 3001),
+      port: +(process.env.PORT ?? 3000),
       host: process.env.HOST,
       // The `gracePeriodForClose` provides a graceful close for http/https
       // servers with keep-alive clients. The default value is `Infinity`
