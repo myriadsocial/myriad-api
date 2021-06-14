438c4bebe02a2c961b1364f822f24f551ced3622
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-06-15 00:58:43 +0700

    fix env firebase

--- src/application.ts
+++ src/application.ts
@@ -109,9 +109,9 @@ export class MyriadApiApplication extends BootMixin(
     // initialize firebase app
     firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: (process.env.FIREBASE_PROJECT_ID ?? ""),
+        clientEmail: (process.env.FIREBASE_CLIENT_EMAIL ?? ""),
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY ?? "").replace(/\\n/g, '\n'),
       }),
     })
   }
