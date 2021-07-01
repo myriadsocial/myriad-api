d20f00e10e0b6ab976142e8da2b9dfbf96ca5ef3
abdulhakim2902 <hakim@blocksphere.id>
2021-07-01 15:59:53 +0700

    bug fix transactions

--- src/application.ts
+++ src/application.ts
@@ -114,16 +114,98 @@ export class MyriadApiApplication extends BootMixin(
   async migrateSchema(options?: SchemaMigrationOptions) {
     await super.migrateSchema(options)
 
-    const tagRepo = await this.getRepository(TagRepository)
-    const postsRepo = await this.getRepository(PostRepository)
-    const peopleRepo = await this.getRepository(PeopleRepository)
-    const transactionRepo = await this.getRepository(TransactionRepository)
-    const userRepo = await this.getRepository(UserRepository)
-    const savedExperienceRepo = await this.getRepository(SavedExperienceRepository)
-    const experienceRepo = await this.getRepository(ExperienceRepository)
-    const userCredRepo = await this.getRepository(UserCredentialRepository)
-    const commentRepo = await this.getRepository(CommentRepository)
-    const publicMetricRepo = await this.getRepository(PublicMetricRepository)
+    const {
+      userRepository, 
+      tokenRepository, 
+      userTokenRepository, 
+      peopleRepository,
+      tagRepository,
+      postRepository,
+      publicMetricRepository
+    } = await this.getAllRepository();
+
+    const updatedUsers = this.updateUsers(users);
+    const updatedTokens = tokens;
+
+    const newToken = await tokenRepository.createAll(updatedTokens)
+    const newUsers = await userRepository.createAll(updatedUsers)
+    const newPeople = await peopleRepository.createAll(people)
+
+    const updatedPost = this.updatePost(newPeople, posts);
+
+    for (let i = 0; i < newUsers.length; i++) {
+      await userTokenRepository.create({
+        userId: newUsers[i].id,
+        tokenId: "MYR"
+      })
+    }
+
+    for (let i = 0; i < updatedPost.length; i++) {
+      const {tags} = updatedPost[i]
+      const post: any = updatedPost[i]
+      const newKey = this.createKeyring().addFromUri('//' + post.peopleId)
+
+      post.walletAddress = u8aToHex(newKey.publicKey);
+
+      const newPost = await postRepository.create(post);
+
+      await publicMetricRepository.create({
+        liked: 0,
+        comment: 0,
+        disliked: 0,
+        postId: newPost.id
+      })
+
+      for (let j = 0; j < tags.length; j++) {
+        const foundTag = await tagRepository.findOne({
+          where: {
+            or: [
+              {
+                id: tags[j]
+              },
+              {
+                id: tags[j].toLowerCase()
+              },
+              {
+                id: tags[j].toUpperCase()
+              }
+            ],
+          }
+        })
+
+        if (!foundTag) {
+          await tagRepository.create({
+            id: tags[j],
+            count: 1,
+            createdAt: new Date().toString(),
+            updatedAt: new Date().toString()
+          })
+        } else {
+          const oneDay: number = 60 * 60 * 24 * 1000;
+          const isOneDay: boolean = new Date().getTime()
+            - new Date(foundTag.updatedAt).getTime()
+            > oneDay;
+
+          await tagRepository.updateById(foundTag.id, {
+            updatedAt: new Date().toString(),
+            count: isOneDay ? 1 : foundTag.count + 1
+          })
+        }
+      }
+    }
+  }
+
+  async getAllRepository() {
+    const tagRepository = await this.getRepository(TagRepository)
+    const postRepository = await this.getRepository(PostRepository)
+    const peopleRepository = await this.getRepository(PeopleRepository)
+    const transactionRepository = await this.getRepository(TransactionRepository)
+    const userRepository = await this.getRepository(UserRepository)
+    const savedExperienceRepository = await this.getRepository(SavedExperienceRepository)
+    const experienceRepository = await this.getRepository(ExperienceRepository)
+    const userCredRepository = await this.getRepository(UserCredentialRepository)
+    const commentRepository = await this.getRepository(CommentRepository)
+    const publicMetricRepository = await this.getRepository(PublicMetricRepository)
     const likeRepository = await this.getRepository(LikeRepository)
     const conversationRepository = await this.getRepository(ConversationRepository)
     const friendRepository = await this.getRepository(FriendRepository)
@@ -137,16 +219,16 @@ export class MyriadApiApplication extends BootMixin(
 
     await likeRepository.deleteAll()
     await conversationRepository.deleteAll()
-    await tagRepo.deleteAll()
-    await postsRepo.deleteAll()
-    await peopleRepo.deleteAll()
-    await transactionRepo.deleteAll()
-    await userRepo.deleteAll()
-    await savedExperienceRepo.deleteAll()
-    await experienceRepo.deleteAll()
-    await userCredRepo.deleteAll()
-    await commentRepo.deleteAll()
-    await publicMetricRepo.deleteAll()
+    await tagRepository.deleteAll()
+    await postRepository.deleteAll()
+    await peopleRepository.deleteAll()
+    await transactionRepository.deleteAll()
+    await userRepository.deleteAll()
+    await savedExperienceRepository.deleteAll()
+    await experienceRepository.deleteAll()
+    await userCredRepository.deleteAll()
+    await commentRepository.deleteAll()
+    await publicMetricRepository.deleteAll()
     await friendRepository.deleteAll()
     await tokenRepository.deleteAll()
     await userTokenRepository.deleteAll()
@@ -156,17 +238,26 @@ export class MyriadApiApplication extends BootMixin(
     await authCredentialRepository.deleteAll()
     await refreshTokenRepository.deleteAll()
 
-    const keyring = new Keyring({
-      type: process.env.MYRIAD_CRYPTO_TYPE as KeypairType
-    });
+    return {
+      tokenRepository,
+      userRepository,
+      userTokenRepository,
+      peopleRepository,
+      postRepository,
+      publicMetricRepository,
+      tagRepository
+    }
+  }
 
-    const updateUsers = users.map((user: any) => {
+  updateUsers(users: object[]) {
+    const updatedUsers = users.map((user: any) => {
       const seed = mnemonicGenerate()
-      const pair = keyring.createFromUri(seed + '', user)
+      const pair = this.createKeyring().createFromUri(seed + '', user)
       const name = user.name
 
       return {
         ...user,
+        username: name.toLowerCase(),
         id: u8aToHex(pair.publicKey),
         seed_example: seed,
         bio: `Hello, my name is ${name}`,
@@ -175,55 +266,12 @@ export class MyriadApiApplication extends BootMixin(
       }
     })
 
-    const newToken = await tokenRepository.createAll(tokens)
-    const newUser = await userRepo.createAll(updateUsers)
+    return updatedUsers
+  }
 
-    // const api = await polkadotApi(process.env.MYRIAD_WS_RPC || "") 
-
-    // for (let i = 0; i < updateUsers.length; i++) {
-    //   const mnemonic = process.env.MYRIAD_FAUCET_MNEMONIC ?? "";
-    //   const from = keyring.addFromMnemonic(mnemonic);
-    //   const value = +(process.env.MYRIAD_ACCOUNT_DEPOSIT ?? 100000000000000);
-    //   const myriadPrefix = Number(process.env.MYRIAD_ADDRESS_PREFIX);
-    //   const {nonce} = await api.query.system.account(encodeAddress(from.address, myriadPrefix))
-
-    //   let count: number = nonce.toJSON()
-
-    //   const transfer = api.tx.balances.transfer(encodeAddress(updateUsers[i].id, myriadPrefix), value)
-
-    //   const newUser = await userRepo.create(updateUsers[i])
-    //   const txHash = await transfer.signAndSend(from, {nonce: count + i});
-
-    //   await transactionRepo.create({
-    //     trxHash: txHash.toString(),
-    //     from: u8aToHex(from.publicKey),
-    //     to: updateUsers[i].id,
-    //     value: value,
-    //     state: 'success',
-    //     createdAt: new Date().toString(),
-    //     tokenId: 'MYR',
-    //     hasSendToUser: true
-    //   })
-
-    //   await userTokenRepository.create({
-    //     userId: newUser.id,
-    //     tokenId: "MYR"
-    //   })
-
-    //   await detailTransactionRepository.create({
-    //     sentToMe: 100000000000000,
-    //     sentToThem: 0,
-    //     userId: newUser.id,
-    //     tokenId: 'MYR'
-    //   })
-    // }
-
-    // await api.disconnect()
-
-    const newPeople = await peopleRepo.createAll(people)
-
-    for (let i = 0; i < newPeople.length; i++) {
-      const person = newPeople[i]
+  updatePost(people:any[], posts: any[]) {
+    for (let i = 0; i < people.length; i++) {
+      const person = people[i]
       const personAccountId = person.platform_account_id
       const personUsername = person.username
       const personPlatform = person.platform
@@ -256,58 +304,12 @@ export class MyriadApiApplication extends BootMixin(
       }
     }
 
-    for (let i = 0; i < posts.length; i++) {
-      const {tags} = posts[i]
-      const post: any = posts[i]
-      const newKey = keyring.addFromUri('//' + post.peopleId)
+    return posts
+  }
 
-      post.walletAddress = u8aToHex(newKey.publicKey);
-
-      const newPost = await postsRepo.create(post);
-
-      await publicMetricRepo.create({
-        liked: 0,
-        comment: 0,
-        disliked: 0,
-        postId: newPost.id
-      })
-
-      for (let j = 0; j < tags.length; j++) {
-        const foundTag = await tagRepo.findOne({
-          where: {
-            or: [
-              {
-                id: tags[j]
-              },
-              {
-                id: tags[j].toLowerCase()
-              },
-              {
-                id: tags[j].toUpperCase()
-              }
-            ],
-          }
-        })
-
-        if (!foundTag) {
-          await tagRepo.create({
-            id: tags[j],
-            count: 1,
-            createdAt: new Date().toString(),
-            updatedAt: new Date().toString()
-          })
-        } else {
-          const oneDay: number = 60 * 60 * 24 * 1000;
-          const isOneDay: boolean = new Date().getTime()
-            - new Date(foundTag.updatedAt).getTime()
-            > oneDay;
-
-          await tagRepo.updateById(foundTag.id, {
-            updatedAt: new Date().toString(),
-            count: isOneDay ? 1 : foundTag.count + 1
-          })
-        }
-      }
-    }
+  createKeyring() {
+    return new Keyring({
+      type: process.env.MYRIAD_CRYPTO_TYPE as KeypairType
+    });
   }
 }
