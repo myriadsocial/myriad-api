d80742ce173a01ea2e7e02babc6b11402cea8f7a
abdulhakim2902 <hakim@blocksphere.id>
2021-07-12 21:58:47 +0700

    tip.repository.ts: add new relation tip belong to cryptocurrency

--- src/repositories/tip.repository.ts
+++ src/repositories/tip.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {Tip, TipRelations} from '../models';
+import {Tip, TipRelations, Cryptocurrency} from '../models';
+import {CryptocurrencyRepository} from './cryptocurrency.repository';
 
 export class TipRepository extends DefaultCrudRepository<
   Tip,
   typeof Tip.prototype.id,
   TipRelations
 > {
+
+  public readonly cryptocurrency: BelongsToAccessor<Cryptocurrency, typeof Tip.prototype.id>;
+
   constructor(
-    @inject('datasources.mongo') dataSource: MongoDataSource,
+    @inject('datasources.mongo') dataSource: MongoDataSource, @repository.getter('CryptocurrencyRepository') protected cryptocurrencyRepositoryGetter: Getter<CryptocurrencyRepository>,
   ) {
     super(Tip, dataSource);
+    this.cryptocurrency = this.createBelongsToAccessorFor('cryptocurrency', cryptocurrencyRepositoryGetter,);
+    this.registerInclusionResolver('cryptocurrency', this.cryptocurrency.inclusionResolver);
   }
 }
