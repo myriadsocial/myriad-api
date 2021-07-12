6c0e9c8d33a751ab9dca662b4f9ec4230bcea8b0
abdulhakim2902 <hakim@blocksphere.id>
2021-07-12 21:57:44 +0700

    token.repository.ts: change token to cryptocurrency

---
+++ src/repositories/cryptocurrency.repository.ts
@@ -0,0 +1,36 @@
+import {Getter, inject} from '@loopback/core';
+import {
+  DefaultCrudRepository,
+  HasManyRepositoryFactory,
+  repository
+} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {TransactionHistory, Cryptocurrency, CryptocurrencyRelations, Transaction, Tip} from '../models';
+import { TipRepository } from './tip.repository';
+import {TransactionHistoryRepository} from './transaction-history.repository';
+import {TransactionRepository} from './transaction.repository';
+
+export class CryptocurrencyRepository extends DefaultCrudRepository<
+  Cryptocurrency,
+  typeof Cryptocurrency.prototype.id,
+  CryptocurrencyRelations
+> {
+
+  public readonly transactions: HasManyRepositoryFactory<Transaction, typeof Cryptocurrency.prototype.id>;
+
+  public readonly transactionHistories: HasManyRepositoryFactory<TransactionHistory, typeof Cryptocurrency.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource,
+    @repository.getter('TransactionRepository')
+    protected transactionRepositoryGetter: Getter<TransactionRepository>,
+    @repository.getter('TransactionHistoryRepository')
+    protected transactionHistoryRepositoryGetter: Getter<TransactionHistoryRepository>,
+  ) {
+    super(Cryptocurrency, dataSource);
+    this.transactionHistories = this.createHasManyRepositoryFactoryFor('transactionHistories', transactionHistoryRepositoryGetter,);
+    this.registerInclusionResolver('transactionHistories', this.transactionHistories.inclusionResolver);
+    this.transactions = this.createHasManyRepositoryFactoryFor('transactions', transactionRepositoryGetter,);
+    this.registerInclusionResolver('transactions', this.transactions.inclusionResolver);
+  }
+}
