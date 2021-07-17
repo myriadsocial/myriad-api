a27dc4e188319718d256dc1e10e54920bf6998b4
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:07:18 +0700

    user-crypto.repository.ts: rename UserToken to UserCrypto

---
+++ src/repositories/user-crypto.repository.ts
@@ -0,0 +1,14 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {UserCrypto, UserCryptoRelations} from '../models';
+
+export class UserCryptoRepository extends DefaultCrudRepository<
+  UserCrypto,
+  typeof UserCrypto.prototype.id,
+  UserCryptoRelations
+> {
+  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
+    super(UserCrypto, dataSource);
+  }
+}
