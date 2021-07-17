deeb9e959a0e7aeb0f645b4a85f304bb25dab990
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:21:40 +0700

    fetchContentSocialMediaJob: removing logic for now

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,5 +1,1 @@
-export * from './fetchContentReddit.job';
 export * from './fetchContentSocialMedia.job';
-export * from './fetchContentTwitter.job';
-export * from './removedContent.job';
-export * from './updatePosts.job';
