7dcb7b8958ae6f071c0bdc8789d21435d535ac9c
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:17:37 +0700

    localrpc constant

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,6 @@
 export * from './notification-type.enum';
 export * from './friendstatus-type.enum';
-export * from './platform-type.enum'
+export * from './localrpc.enum';
+export * from './timelinefilter-type.enum';
+export * from './defaultcrypto-type.enum';
+export * from './platform-type.enum';
