a14a67152976d8dd2dc56d01db6ac83066286b73
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 11:44:08 +0700

    prettier fix

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -2,8 +2,12 @@ import {BindingScope, injectable, service} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {NotificationType} from '../enums';
 import {Comment, Notification} from '../models';
-import {NotificationRepository, PostRepository, UserRepository} from '../repositories';
-import { FCMService } from './fcm.service';
+import {
+  NotificationRepository,
+  PostRepository,
+  UserRepository,
+} from '../repositories';
+import {FCMService} from './fcm.service';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class NotificationService {
@@ -16,77 +20,83 @@ export class NotificationService {
     public notificationRepository: NotificationRepository,
     @service(FCMService)
     public fcmService: FCMService,
-  ) { }
+  ) {}
 
   async sendFriendRequest(from: string, to: string): Promise<boolean> {
     const fromUser = await this.userRepository.findById(from);
-    if (fromUser == null) return false
+    if (fromUser == null) return false;
     const toUser = await this.userRepository.findById(to);
-    if (toUser == null) return false
+    if (toUser == null) return false;
 
-    const notification = new Notification()
-    notification.type = NotificationType.FRIEND_REQUEST
-    notification.from = fromUser.id
-    notification.to = toUser.id
-    notification.referenceId = toUser.id
-    notification.message = 'sent you friend request'
-    notification.createdAt = new Date().toString()
+    const notification = new Notification();
+    notification.type = NotificationType.FRIEND_REQUEST;
+    notification.from = fromUser.id;
+    notification.to = toUser.id;
+    notification.referenceId = toUser.id;
+    notification.message = 'sent you friend request';
+    notification.createdAt = new Date().toString();
 
-    const createdNotification = await this.notificationRepository.create(notification)
-    if (createdNotification == null) return false
+    const createdNotification = await this.notificationRepository.create(
+      notification,
+    );
+    if (createdNotification == null) return false;
 
-    const title = 'Friend Request Accepted'
-    const body = fromUser.name + ' ' + notification.message
-    await this.fcmService.sendNotification(toUser.fcmTokens, title, body)
+    const title = 'Friend Request Accepted';
+    const body = fromUser.name + ' ' + notification.message;
+    await this.fcmService.sendNotification(toUser.fcmTokens, title, body);
 
-    return true
+    return true;
   }
 
   async sendFriendAccept(from: string, to: string): Promise<boolean> {
     const fromUser = await this.userRepository.findById(from);
-    if (fromUser == null) return false
+    if (fromUser == null) return false;
     const toUser = await this.userRepository.findById(to);
-    if (toUser == null) return false
+    if (toUser == null) return false;
 
-    const notification = new Notification()
-    notification.type = NotificationType.FRIEND_ACCEPT
-    notification.from = fromUser.id
-    notification.to = toUser.id
-    notification.referenceId = toUser.id
-    notification.message = 'accept your friend request'
-    notification.createdAt = new Date().toString()
+    const notification = new Notification();
+    notification.type = NotificationType.FRIEND_ACCEPT;
+    notification.from = fromUser.id;
+    notification.to = toUser.id;
+    notification.referenceId = toUser.id;
+    notification.message = 'accept your friend request';
+    notification.createdAt = new Date().toString();
 
-    const createdNotification = await this.notificationRepository.create(notification)
-    if (createdNotification == null) return false
+    const createdNotification = await this.notificationRepository.create(
+      notification,
+    );
+    if (createdNotification == null) return false;
 
-    const title = 'Friend Request Accepted'
-    const body = fromUser.name + ' ' + notification.message
-    await this.fcmService.sendNotification(toUser.fcmTokens, title, body)
+    const title = 'Friend Request Accepted';
+    const body = fromUser.name + ' ' + notification.message;
+    await this.fcmService.sendNotification(toUser.fcmTokens, title, body);
 
-    return true
+    return true;
   }
 
   async sendPostComment(from: string, comment: Comment): Promise<boolean> {
     const fromUser = await this.userRepository.findById(from);
-    if (fromUser == null) return false
+    if (fromUser == null) return false;
     const toUser = await this.postRepository.user(comment.postId);
-    if (toUser == null) return false
+    if (toUser == null) return false;
 
-    const notification = new Notification()
-    notification.type = NotificationType.POST_COMMENT
-    notification.from = fromUser.id
-    notification.to = toUser.id
-    notification.referenceId = comment.id
-    notification.message = 'commented: ' + comment.text
-    notification.createdAt = new Date().toString()
+    const notification = new Notification();
+    notification.type = NotificationType.POST_COMMENT;
+    notification.from = fromUser.id;
+    notification.to = toUser.id;
+    notification.referenceId = comment.id;
+    notification.message = 'commented: ' + comment.text;
+    notification.createdAt = new Date().toString();
 
-    const createdNotification = await this.notificationRepository.create(notification)
-    if (createdNotification == null) return false
+    const createdNotification = await this.notificationRepository.create(
+      notification,
+    );
+    if (createdNotification == null) return false;
 
-    const title = 'New Comment'
-    const body = fromUser.name + ' ' + notification.message
-    await this.fcmService.sendNotification(toUser.fcmTokens, title, body)
+    const title = 'New Comment';
+    const body = fromUser.name + ' ' + notification.message;
+    await this.fcmService.sendNotification(toUser.fcmTokens, title, body);
 
-    return true
+    return true;
   }
 }
