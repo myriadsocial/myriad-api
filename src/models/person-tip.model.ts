87b0e58ca0aeac107bc55f89b52d7f357f6c84f9
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 08:30:28 +0700

    Rename model: Token -> Cryptocurrency, DetailTransaction -> TransactionHistory, Tip -> PersonTip, UserToken -> UserCrypto

---
+++ src/models/person-tip.model.ts
@@ -0,0 +1,54 @@
+// TODO: Change Tip model to PersonTip model
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Cryptocurrency} from './cryptocurrency.model';
+
+@model({
+  settings: {
+    strictObjectIDCoercion: true,
+    mongodb: {
+      collection: 'personTips',
+    },
+    hiddenProperties: ['total'],
+  },
+})
+export class PersonTip extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+    mongodb: {
+      dataType: 'ObjectId',
+    },
+  })
+  id?: string;
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'string',
+  })
+  peopleId?: string;
+
+  @property({
+    type: 'object',
+    required: false,
+  })
+  cryptocurrency: Cryptocurrency;
+
+  @belongsTo(() => Cryptocurrency)
+  cryptocurrencyId: string;
+
+  constructor(data?: Partial<PersonTip>) {
+    super(data);
+  }
+}
+
+export interface PersonTipRelations {
+  // describe navigational properties here
+  // cryptocurrency: Cryptocurrency;
+}
+
+export type PersonTipWithRelations = PersonTip & PersonTipRelations;
