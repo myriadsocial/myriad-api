ae875ebf5d328a5923d522af6bbd625bcbf921a7
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:11:29 +0700

    transaction.repository.ts: rename variable token to cryptocurrency

--- src/repositories/transaction.repository.ts
+++ src/repositories/transaction.repository.ts
@@ -2,12 +2,16 @@ import {Getter, inject} from '@loopback/core';
 import {
   BelongsToAccessor,
   DefaultCrudRepository,
-  repository
+  repository,
 } from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {Post, Token, Transaction, TransactionRelations, User} from '../models';
-import {PostRepository} from './post.repository';
-import {TokenRepository} from './token.repository';
+import {
+  Cryptocurrency,
+  Transaction,
+  TransactionRelations,
+  User,
+} from '../models';
+import {CryptocurrencyRepository} from './cryptocurrency.repository';
 import {UserRepository} from './user.repository';
 
 export class TransactionRepository extends DefaultCrudRepository<
@@ -15,32 +19,46 @@ export class TransactionRepository extends DefaultCrudRepository<
   typeof Transaction.prototype.id,
   TransactionRelations
 > {
+  public readonly fromUser: BelongsToAccessor<
+    User,
+    typeof Transaction.prototype.id
+  >;
 
-  public readonly fromUser: BelongsToAccessor<User, typeof Transaction.prototype.id>;
+  public readonly toUser: BelongsToAccessor<
+    User,
+    typeof Transaction.prototype.id
+  >;
 
-  public readonly toUser: BelongsToAccessor<User, typeof Transaction.prototype.id>;
-
-  public readonly token: BelongsToAccessor<Token, typeof Transaction.prototype.id>;
-
-  public readonly post: BelongsToAccessor<Post, typeof Transaction.prototype.id>;
+  public readonly cryptocurrency: BelongsToAccessor<
+    Cryptocurrency,
+    typeof Transaction.prototype.id
+  >;
 
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
     @repository.getter('UserRepository')
     protected userRepositoryGetter: Getter<UserRepository>,
-    @repository.getter('TokenRepository')
-    protected tokenRepositoryGetter: Getter<TokenRepository>,
-    @repository.getter('PostRepository')
-    protected postRepositoryGetter: Getter<PostRepository>,
+    @repository.getter('CryptocurrencyRepository')
+    protected cryptocurrencyRepositoryGetter: Getter<CryptocurrencyRepository>,
   ) {
     super(Transaction, dataSource);
-    this.post = this.createBelongsToAccessorFor('post', postRepositoryGetter,);
-    this.registerInclusionResolver('post', this.post.inclusionResolver);
-    this.token = this.createBelongsToAccessorFor('token', tokenRepositoryGetter,);
-    this.registerInclusionResolver('token', this.token.inclusionResolver);
-    this.fromUser = this.createBelongsToAccessorFor('fromUser', userRepositoryGetter,);
+    this.cryptocurrency = this.createBelongsToAccessorFor(
+      'cryptocurrency',
+      cryptocurrencyRepositoryGetter,
+    );
+    this.registerInclusionResolver(
+      'cryptocurrency',
+      this.cryptocurrency.inclusionResolver,
+    );
+    this.fromUser = this.createBelongsToAccessorFor(
+      'fromUser',
+      userRepositoryGetter,
+    );
     this.registerInclusionResolver('fromUser', this.fromUser.inclusionResolver);
-    this.toUser = this.createBelongsToAccessorFor('toUser', userRepositoryGetter,);
+    this.toUser = this.createBelongsToAccessorFor(
+      'toUser',
+      userRepositoryGetter,
+    );
     this.registerInclusionResolver('toUser', this.toUser.inclusionResolver);
   }
 }
