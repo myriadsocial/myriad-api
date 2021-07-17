40eab1752dbfb62f8970202c248e38ff570ac86f
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 10:58:31 +0700

    moved all social media api related to platform folder

--- src/services/platform/reddit.service.ts
+++ src/services/platform/reddit.service.ts
@@ -1,12 +1,12 @@
 import {inject, Provider} from '@loopback/core';
 import {getService} from '@loopback/service-proxy';
-import {RedditDataSource} from '../datasources';
+import {RedditDataSource} from '../../datasources';
 
 export interface Reddit {
   // this is where you define the Node.js methods that will be
   // mapped to REST/SOAP/gRPC operations as stated in the datasource
   // json file.
-  getActions(action: String): Promise<any>
+  getActions(action: String): Promise<any>;
 }
 
 export class RedditProvider implements Provider<Reddit> {
@@ -14,7 +14,7 @@ export class RedditProvider implements Provider<Reddit> {
     // reddit must match the name property in the datasource json file
     @inject('datasources.reddit')
     protected dataSource: RedditDataSource = new RedditDataSource(),
-  ) { }
+  ) {}
 
   value(): Promise<Reddit> {
     return getService(this.dataSource);
