20696f0b785245fd11236afd95097bc0dca6d400
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:29:00 +0700

    helpers: prettier fix

--- src/helpers/polkadotApi.ts
+++ src/helpers/polkadotApi.ts
@@ -2,11 +2,11 @@ import {ApiPromise, WsProvider} from '@polkadot/api';
 
 export async function polkadotApi(wssProvider: string): Promise<ApiPromise> {
   try {
-    const provider = new WsProvider(wssProvider)
-    const api = await new ApiPromise({provider}).isReadyOrError
+    const provider = new WsProvider(wssProvider);
+    const api = await new ApiPromise({provider}).isReadyOrError;
 
-    return api
+    return api;
   } catch (e) {
-    throw new Error('LostConnection')
+    throw new Error('LostConnection');
   }
 }
