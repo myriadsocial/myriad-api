5736f034a30f3dfa3195e4d84bef44c39619d89a
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 11:44:53 +0700

    tag.service.ts: add new service, counting topic mention

---
+++ src/services/tag.service.ts
@@ -0,0 +1,49 @@
+import {repository} from '@loopback/repository';
+import {Tag} from '../models';
+import {TagRepository} from '../repositories';
+
+export class TagService {
+  constructor(
+    @repository(TagRepository)
+    protected tagRepository: TagRepository,
+  ) {}
+
+  async createTags(tags: string[]): Promise<void> {
+    for (const tag of tags) {
+      const foundTag = await this.tagRepository.findOne({
+        where: {
+          or: [
+            {
+              id: tag,
+            },
+            {
+              id: tag.toLowerCase(),
+            },
+            {
+              id: tag.toUpperCase(),
+            },
+          ],
+        },
+      });
+
+      if (!foundTag) {
+        this.tagRepository.create({
+          id: tag,
+          count: 1,
+          createdAt: new Date().toString(),
+          updatedAt: new Date().toString(),
+        }) as Promise<Tag>;
+      } else {
+        const oneDay: number = 60 * 60 * 24 * 1000;
+        const isOneDay: boolean =
+          new Date().getTime() - new Date(foundTag.updatedAt).getTime() >
+          oneDay;
+
+        this.tagRepository.updateById(foundTag.id, {
+          updatedAt: new Date().toString(),
+          count: isOneDay ? 1 : foundTag.count + 1,
+        }) as Promise<void>;
+      }
+    }
+  }
+}
