5916c2f61149968d0ceb02ea811eb64bc388cbd1
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 11:42:47 +0700

    prettier fix

--- src/services/fcm.service.ts
+++ src/services/fcm.service.ts
@@ -3,10 +3,14 @@ import * as firebaseAdmin from 'firebase-admin';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class FCMService {
-  constructor() { }
+  constructor() {}
 
-  async sendNotification(fcmTokens?: string[], title?: string, body?: string): Promise<void> {
-    if (fcmTokens == null || fcmTokens.length <= 0) return
+  async sendNotification(
+    fcmTokens?: string[],
+    title?: string,
+    body?: string,
+  ): Promise<void> {
+    if (fcmTokens == null || fcmTokens.length <= 0) return;
 
     const message = {
       notification: {
@@ -16,8 +20,8 @@ export class FCMService {
       tokens: fcmTokens,
     };
 
-    let result = await firebaseAdmin.messaging().sendMulticast(message);
+    await firebaseAdmin.messaging().sendMulticast(message);
 
-    return
+    return;
   }
 }
