659a204726e07ddbba418c2303f6506b88c7cc8e
abdulhakim2902 <hakim@blocksphere.id>
2021-07-17 09:17:16 +0700

    timelinefilter-type.enum.ts: new enum for filtering

---
+++ src/enums/timelinefilter-type.enum.ts
@@ -0,0 +1,19 @@
+export enum OrderFieldType {
+  COMMENT = 'total_comment',
+  LIKED = 'total_liked',
+  DISLIKED = 'total_disliked',
+  PLATFORMCREATEDAT = 'platform_created_at',
+  CREATEDAT = 'created_at',
+  UPDATEDAT = 'updated_at',
+  COUNT = 'count',
+}
+
+export enum OrderType {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export enum DefaultInq {
+  TAGS = 'cryptocurrency,blockchain,technology',
+  PEOPLE = 'elonmusk,gavofyork,W3F_Bill,CryptoChief,BillGates,vitalikbuterineth',
+}
