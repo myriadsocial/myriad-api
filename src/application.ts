dfecbfac2ae0d51e72d1a4dae548b9431c8a2487
abdulhakim2902 <hakim@blocksphere.id>
2021-07-23 13:23:54 +0700

    fixed

--- src/application.ts
+++ src/application.ts
@@ -101,19 +101,20 @@ export class MyriadApiApplication extends BootMixin(
   }
 
   firebaseInit() {
-    try {
+    if (
+      !process.env.FIREBASE_PROJECT_ID ||
+      !process.env.FIREBASE_CLIENT_EMAIL ||
+      !process.env.FIREBASE_PRIVATE_KEY
+    ) {
+      firebaseAdmin.initializeApp();
+    } else {
       firebaseAdmin.initializeApp({
         credential: firebaseAdmin.credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID ?? '',
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? '',
-          privateKey: (process.env.FIREBASE_PRIVATE_KEY ?? '').replace(
-            /\\n/g,
-            '\n',
-          ),
+          privateKey: (process.env.FIREBASE_PRIVATE_KEY ?? '').replace(/\\n/g, '\n'),
         }),
       });
-    } catch {
-      firebaseAdmin.initializeApp();
     }
   }
 }
