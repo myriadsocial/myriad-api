1b909eca9422bcea22d7740fae68762ae1e1c04f
abdulhakim2902 <hakim@blocksphere.id>
2021-07-26 15:41:00 +0700

    add user-agent

--- src/datasources/reddit.datasource.ts
+++ src/datasources/reddit.datasource.ts
@@ -8,7 +8,8 @@ const config = {
   crud: false,
   options: {
     headers: {
-
+      "content-type": "application/json",
+      "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
     }
   },
   operations: [
