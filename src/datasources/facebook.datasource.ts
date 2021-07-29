655f55683d7a0f02d115f81607f540b0a4b1db3a
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-07-29 21:47:46 +0700

    fb-datasource: use header metadata

--- src/datasources/facebook.datasource.ts
+++ src/datasources/facebook.datasource.ts
@@ -6,11 +6,17 @@ const config = {
   connector: 'rest',
   baseURL: 'https://mbasic.facebook.com',
   crud: false,
+  options: {
+    headers: {
+      "content-type": "text/html",
+      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+    }
+  },
   operations: [
     {
       template: {
         method: 'GET',
-        url: 'https://facebook.com/{pageId}/posts/{postId}',
+        url: 'https://mbasic.facebook.com/{pageId}/posts/{postId}',
       },
       functions: {
         getActions: ['pageId', 'postId'],
