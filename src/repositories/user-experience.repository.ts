9d6c23fe0512804bdbc6400865cf87073e751f56
abdulhakim2902 <hakim@blocksphere.id>
2021-07-30 09:46:51 +0700

    change savedExperience Repository to userExperienceRepository

---
+++ src/repositories/user-experience.repository.ts
@@ -0,0 +1,28 @@
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, DefaultCrudRepository, repository} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {Experience, UserExperience, UserExperienceRelations, User} from '../models';
+import {ExperienceRepository} from './experience.repository';
+import {UserRepository} from './user.repository';
+
+export class UserExperienceRepository extends DefaultCrudRepository<
+  UserExperience,
+  typeof UserExperience.prototype.id,
+  UserExperienceRelations
+> {
+  public readonly experience: BelongsToAccessor<Experience, typeof UserExperience.prototype.id>;
+
+  public readonly user: BelongsToAccessor<User, typeof UserExperience.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource,
+    @repository.getter('ExperienceRepository')
+    protected experienceRepositoryGetter: Getter<ExperienceRepository>, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
+  ) {
+    super(UserExperience, dataSource);
+    this.user = this.createBelongsToAccessorFor('user', userRepositoryGetter,);
+    this.registerInclusionResolver('user', this.user.inclusionResolver);
+    this.experience = this.createBelongsToAccessorFor('experience', experienceRepositoryGetter);
+    this.registerInclusionResolver('experience', this.experience.inclusionResolver);
+  }
+}
