f605341f6f606f920877f78f9b3b4522b31babb9
abdulhakim2902 <hakim@blocksphere.id>
2021-07-31 13:00:40 +0700

    user-experience.model.ts: add new field (clonedFrom, updateStatus)

--- src/models/user-experience.model.ts
+++ src/models/user-experience.model.ts
@@ -1,4 +1,5 @@
 import {belongsTo, Entity, model, property} from '@loopback/repository';
+import { UpdatedStatusType } from '../enums';
 import {Experience} from './experience.model';
 import {User} from './user.model';
 
@@ -23,11 +24,27 @@ export class UserExperience extends Entity {
     },
   })
   id: string;
+
   @property({
     type: 'boolean',
     default: false,
   })
   hasSelected: boolean;
+
+  @property({
+    type: 'string',
+    default: null,
+  })
+  clonedFrom?: string | null;
+
+  @property({
+    type: 'string',
+    default: UpdatedStatusType.NONE,
+    jsonSchema: {
+      enum: Object.values(UpdatedStatusType),
+    },
+  })
+  updateStatus?: UpdatedStatusType;
 
   @belongsTo(() => Experience)
   experienceId: string;
