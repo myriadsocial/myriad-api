db4f7f360f716536b9bc90662623bad85b1a9eda
abdulhakim2902 <hakim@blocksphere.id>
2021-08-01 19:48:21 +0700

    refactor polkadotJs

---
+++ src/helpers/polkadotJs-utils.ts
@@ -0,0 +1,39 @@
+import {ApiPromise, Keyring, WsProvider} from '@polkadot/api';
+import {KeyringPair} from '@polkadot/keyring/types';
+import {u8aToHex} from '@polkadot/util';
+import {mnemonicGenerate} from '@polkadot/util-crypto';
+import {KeypairType} from '@polkadot/util-crypto/types';
+
+export class PolkadotJs {
+  provider: string;
+
+  constructor(wssProvider?: string) {
+    this.provider = wssProvider || '';
+  }
+
+  async polkadotApi(): Promise<ApiPromise> {
+    try {
+      const provider = new WsProvider(this.provider);
+      const api = await new ApiPromise({provider}).isReadyOrError;
+
+      return api;
+    } catch (e) {
+      throw new Error('LostConnection');
+    }
+  }
+
+  getKeyring(type?: string, addressFormat?: number): Keyring {
+    return new Keyring({
+      type: type as KeypairType | '' as KeypairType,
+      ss58Format: addressFormat,
+    });
+  }
+
+  getHexPublicKey(keyring: KeyringPair): string {
+    return u8aToHex(keyring.publicKey);
+  }
+
+  generateSeed(): string {
+    return mnemonicGenerate();
+  }
+}
