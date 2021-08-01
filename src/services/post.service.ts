e2951490e055edf44b61887d4e76a736a0b85242
abdulhakim2902 <hakim@blocksphere.id>
2021-08-01 19:43:40 +0700

    post.service.ts: refactor

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,15 +1,10 @@
 import {repository} from '@loopback/repository';
-import {PeopleRepository, PostRepository, PostTipRepository} from '../repositories';
 import {HttpErrors} from '@loopback/rest';
-import {DetailUrl, ExtendedPost} from '../interfaces';
+import {DefaultCryptocurrencyType} from '../enums';
+import {PolkadotJs} from '../helpers/polkadotJs-utils';
+import {ExtendedPost} from '../interfaces';
 import {Post, PostTip, PublicMetric} from '../models';
-import {service} from '@loopback/core';
-import {SocialMediaService} from './social-media.service';
-import {TagService} from './tag.service';
-import {KeypairType} from '@polkadot/util-crypto/types';
-import {u8aToHex} from '@polkadot/util';
-import {Keyring} from '@polkadot/api';
-import {DefaultCrypto, PlatformType} from '../enums';
+import {PeopleRepository, PostRepository, PostTipRepository} from '../repositories';
 
 export class PostService {
   constructor(
@@ -19,113 +14,30 @@ export class PostService {
     protected peopleRepository: PeopleRepository,
     @repository(PostTipRepository)
     protected postTipRepository: PostTipRepository,
-    @service(SocialMediaService)
-    protected socialMediaService: SocialMediaService,
-    @service(TagService)
-    protected tagService: TagService,
   ) {}
-
-  async getPostFromSocialMediaPost(detailUrl: DetailUrl): Promise<Post> {
-    const {textId, platform, postTags, importer, username} = detailUrl;
-    const foundPost = await this.postRepository.findOne({
-      where: {textId, platform},
-    });
-
-    if (foundPost) {
-      const foundImporter = foundPost.importBy.find((userId: string) => userId === importer);
-
-      if (foundImporter) {
-        throw new HttpErrors.UnprocessableEntity('You have already import this post');
-      }
-
-      foundPost.importBy.push(importer);
-
-      this.postRepository.updateById(foundPost.id, {
-        importBy: foundPost.importBy,
-      }) as Promise<void>;
-
-      return foundPost;
-    }
-
-    let newPost: ExtendedPost;
-    let tags: string[] = postTags;
-
-    switch (platform) {
-      case PlatformType.TWITTER: {
-        newPost = await this.socialMediaService.fetchTweet(textId);
-        tags = !newPost.tags
-          ? []
-          : newPost.tags.filter((tag: string) => {
-              return !postTags
-                .map((postTag: string) => postTag.toLowerCase())
-                .includes(tag.toLowerCase());
-            });
-        tags = [...tags, ...postTags];
-
-        break;
-      }
-
-      case PlatformType.REDDIT: {
-        newPost = await this.socialMediaService.fetchRedditPost(textId);
-
-        break;
-      }
-
-      case PlatformType.FACEBOOK: {
-        if (!username) {
-          throw new HttpErrors.UnprocessableEntity('Username not found!');
-        }
-
-        newPost = await this.socialMediaService.fetchFacebookPost(username, textId);
-
-        break;
-      }
-
-      default:
-        throw new HttpErrors.NotFound('Cannot found the specified url!');
-    }
-
-    newPost.tags = tags;
-    newPost.importBy = [importer];
-    newPost.importerId = importer;
-
-    this.tagService.createTags(newPost.tags) as Promise<void>;
-
-    return this.createPost(newPost);
-  }
 
   async createPost(post: Omit<ExtendedPost, 'id'>): Promise<Post> {
     const {platformUser, platform} = post;
+    const {getKeyring, getHexPublicKey} = new PolkadotJs();
 
-    let foundPeople = await this.peopleRepository.findOne({
+    let people = await this.peopleRepository.findOne({
       where: {
         platformAccountId: platformUser?.platformAccountId,
         platform: platform,
       },
     });
 
-    if (!foundPeople) {
-      foundPeople = await this.peopleRepository.create({
-        ...platformUser,
-        platform,
-      });
+    if (!people) {
+      if (platformUser) people = await this.peopleRepository.create(platformUser);
+      throw new HttpErrors.NotFound('Platform user not found!');
     }
 
     delete post.platformUser;
+    post.peopleId = people.id;
 
-    return this.createPostWithPublicMetric({
-      ...post,
-      peopleId: foundPeople.id,
-    });
-  }
+    const newKey = getKeyring(process.env.MYRIAD_CRYPTO_TYPE).addFromUri('//' + post.peopleId);
 
-  async createPostWithPublicMetric(post: Omit<Post, 'id'>): Promise<Post> {
-    const keyring = new Keyring({
-      type: process.env.MYRIAD_CRYPTO_TYPE as KeypairType,
-    });
-    const newKey = keyring.addFromUri('//' + post.peopleId);
-
-    post.walletAddress = u8aToHex(newKey.publicKey);
+    post.walletAddress = getHexPublicKey(newKey);
 
     const createdPost = await this.postRepository.create(post);
 
@@ -133,7 +45,7 @@ export class PostService {
 
     this.postRepository
       .postTips(createdPost.id)
-      .create({cryptocurrencyId: DefaultCrypto.AUSD}) as Promise<PostTip>;
+      .create({cryptocurrencyId: DefaultCryptocurrencyType.AUSD}) as Promise<PostTip>;
 
     return createdPost;
   }
