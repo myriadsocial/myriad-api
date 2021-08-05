937606a29beb701687f7abd84f54dcf602cc037b
abdulhakim2902 <hakim@blocksphere.id>
2021-08-05 16:26:23 +0700

    relation: add relation user hasMany currencies

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,14 +1,39 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {Getter, inject} from '@loopback/core';
+import {
+  DefaultCrudRepository,
+  HasManyThroughRepositoryFactory,
+  repository,
+} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {User, UserRelations} from '../models';
+import {Currency, User, UserCurrency, UserRelations} from '../models';
+import {CurrencyRepository} from './Currency.repository';
+import {UserCurrencyRepository} from './user-currency.repository';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
   UserRelations
 > {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
+  public readonly currencies: HasManyThroughRepositoryFactory<
+    Currency,
+    typeof Currency.prototype.id,
+    UserCurrency,
+    typeof User.prototype.id
+  >;
+
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource,
+    @repository.getter('UserCurrencyRepository')
+    protected userCurrencyRepositoryGetter: Getter<UserCurrencyRepository>,
+    @repository.getter('CurrencyRepository')
+    protected currencyRepositoryGetter: Getter<CurrencyRepository>,
+  ) {
     super(User, dataSource);
+    this.currencies = this.createHasManyThroughRepositoryFactoryFor(
+      'currencies',
+      currencyRepositoryGetter,
+      userCurrencyRepositoryGetter,
+    );
+    this.registerInclusionResolver('currencies', this.currencies.inclusionResolver);
   }
 }
