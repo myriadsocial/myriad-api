aae5dc10d4c09668877252fad8d3588d512db31b
abdulhakim2902 <hakim@blocksphere.id>
2021-08-05 16:08:36 +0700

    refactor people collection

--- src/repositories/people.repository.ts
+++ src/repositories/people.repository.ts
@@ -6,10 +6,9 @@ import {
   repository,
 } from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {People, PeopleRelations, Post, UserCredential, PersonTip} from '../models';
+import {People, PeopleRelations, Post, UserCredential} from '../models';
 import {PostRepository} from './post.repository';
 import {UserCredentialRepository} from './user-credential.repository';
-import {PersonTipRepository} from './person-tip.repository';
 
 export class PeopleRepository extends DefaultCrudRepository<
   People,
@@ -23,23 +22,14 @@ export class PeopleRepository extends DefaultCrudRepository<
 
   public readonly posts: HasManyRepositoryFactory<Post, typeof People.prototype.id>;
 
-  public readonly personTips: HasManyRepositoryFactory<PersonTip, typeof People.prototype.id>;
-
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
     @repository.getter('UserCredentialRepository')
     protected userCredentialRepositoryGetter: Getter<UserCredentialRepository>,
     @repository.getter('PostRepository')
     protected postRepositoryGetter: Getter<PostRepository>,
-    @repository.getter('PersonTipRepository')
-    protected personTipRepositoryGetter: Getter<PersonTipRepository>,
   ) {
     super(People, dataSource);
-    this.personTips = this.createHasManyRepositoryFactoryFor(
-      'personTips',
-      personTipRepositoryGetter,
-    );
-    this.registerInclusionResolver('personTips', this.personTips.inclusionResolver);
     this.posts = this.createHasManyRepositoryFactoryFor('posts', postRepositoryGetter);
     this.registerInclusionResolver('posts', this.posts.inclusionResolver);
     this.userCredential = this.createHasOneRepositoryFactoryFor(
