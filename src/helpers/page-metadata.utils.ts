0f6e5cb0dcea5b30d5a4345e1bc0ddbfe4d5bff0
abdulhakim2902 <hakim@blocksphere.id>
2021-08-05 14:02:29 +0700

    helper for calculate meta data

---
+++ src/helpers/page-metadata.utils.ts
@@ -0,0 +1,34 @@
+import {MetaPagination} from '../interfaces';
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+export function pageMetadata(args: any[], total: number): MetaPagination {
+  let currentPage = +args[0];
+  let currentLimit = args[1] ? +args[1].limit : null;
+
+  if (!currentPage) currentPage = 1;
+  if (!currentLimit) currentLimit = 5;
+
+  const itemsPerPage = currentLimit;
+  const totalItemCount = total;
+  const totalPageCount = Math.ceil(totalItemCount / itemsPerPage);
+
+  const meta: MetaPagination = {
+    totalItemCount,
+    totalPageCount,
+    itemsPerPage,
+  };
+
+  if (currentPage <= meta.totalPageCount) {
+    meta.currentPage = currentPage;
+
+    if (currentPage !== 1) {
+      meta.previousPage = currentPage - 1;
+    }
+
+    if (currentPage !== meta.totalPageCount) {
+      meta.nextPage = meta.currentPage + 1;
+    }
+  }
+
+  return meta;
+}
