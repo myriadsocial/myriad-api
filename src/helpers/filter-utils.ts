a22bf69cc50b4fd8c0dd5fdd89da4da8d9465e69
abdulhakim2902 <hakim@blocksphere.id>
2021-08-05 14:50:59 +0700

    add default filter query

--- src/helpers/filter-utils.ts
+++ src/helpers/filter-utils.ts
@@ -1,3 +1,4 @@
+import {Filter, Where} from '@loopback/repository';
 import {UpdatedStatusType} from '../enums';
 import {Person, Tag} from '../interfaces';
 
@@ -8,4 +9,22 @@ export function noneStatusFiltering(data: Tag[] | Person[]): string[] {
       return false;
     })
     .map(e => e.id);
+}
+
+export function defaultFilterQuery(pageNumber = 1, filter?: Filter, where?: Where): Filter {
+  let itemsPerPage = 5;
+
+  if (!filter) {
+    return {
+      limit: itemsPerPage,
+      skip: (pageNumber - 1) * itemsPerPage,
+    };
+  }
+
+  if (filter.limit) itemsPerPage = filter.limit;
+  if (where) filter.where = where;
+
+  filter.skip = itemsPerPage * (pageNumber - 1);
+
+  return filter;
 }
