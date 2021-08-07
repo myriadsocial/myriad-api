9eee458729427de4dc299b12c6bcee2e764bdbd9
abdulhakim2902 <hakim@blocksphere.id>
2021-08-07 09:42:16 +0700

    user-currency.repository.ts: add relation currency belongs to userCurrency

--- src/repositories/user-currency.repository.ts
+++ src/repositories/user-currency.repository.ts
@@ -1,14 +1,23 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, DefaultCrudRepository, repository} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {UserCurrency, UserCurrencyRelations} from '../models';
+import {Currency, UserCurrency, UserCurrencyRelations} from '../models';
+import {CurrencyRepository} from './currency.repository';
 
 export class UserCurrencyRepository extends DefaultCrudRepository<
   UserCurrency,
   typeof UserCurrency.prototype.id,
   UserCurrencyRelations
 > {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
+  public readonly currency: BelongsToAccessor<Currency, typeof UserCurrency.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource,
+    @repository.getter(CurrencyRepository)
+    protected currencyRepositoryGetter: Getter<CurrencyRepository>,
+  ) {
     super(UserCurrency, dataSource);
+    this.currency = this.createBelongsToAccessorFor('currency', currencyRepositoryGetter);
+    this.registerInclusionResolver('currency', this.currency.inclusionResolver);
   }
 }
