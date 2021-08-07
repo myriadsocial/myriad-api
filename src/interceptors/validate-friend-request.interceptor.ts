695de6459b74ae24d26ca44b91a86b7a151dfcfe
abdulhakim2902 <hakim@blocksphere.id>
2021-08-07 23:46:45 +0700

    validate-friend-request.interceptor.ts: add validation when friend request

---
+++ src/interceptors/validate-friend-request.interceptor.ts
@@ -0,0 +1,52 @@
+import {
+  injectable,
+  Interceptor,
+  InvocationContext,
+  InvocationResult,
+  Provider,
+  service,
+  ValueOrPromise,
+} from '@loopback/core';
+import {FriendService} from '../services';
+
+/**
+ * This class will be bound to the application as an `Interceptor` during
+ * `boot`
+ */
+@injectable({tags: {key: ValidateFriendRequestInterceptor.BINDING_KEY}})
+export class ValidateFriendRequestInterceptor implements Provider<Interceptor> {
+  static readonly BINDING_KEY = `interceptors.${ValidateFriendRequestInterceptor.name}`;
+
+  constructor(
+    @service(FriendService)
+    protected friendService: FriendService,
+  ) {}
+
+  /**
+   * This method is used by LoopBack context to produce an interceptor function
+   * for the binding.
+   *
+   * @returns An interceptor function
+   */
+  value() {
+    return this.intercept.bind(this);
+  }
+
+  /**
+   * The logic to intercept an invocation
+   * @param invocationCtx - Invocation context
+   * @param next - A function to invoke next interceptor or the target method
+   */
+  async intercept(invocationCtx: InvocationContext, next: () => ValueOrPromise<InvocationResult>) {
+    const {requesteeId, requestorId} = invocationCtx.args[0];
+
+    await this.friendService.validateFriendRequest(requesteeId, requestorId);
+
+    invocationCtx.args[0].createdAt = new Date().toString();
+    invocationCtx.args[0].updatedAt = new Date().toString();
+    // Add pre-invocation logic here
+    const result = await next();
+    // Add post-invocation logic here
+    return result;
+  }
+}
