8deb8ece2571ce6ef3af32134e05d3e0d4530f03
abdulhakim2902 <hakim@blocksphere.id>
2021-08-07 09:01:49 +0700

    change user-cryptocurrency to user-currency controller

---
+++ src/controllers/user-currency.controller.ts
@@ -0,0 +1,65 @@
+import {intercept} from '@loopback/core';
+import {Count, CountSchema, repository} from '@loopback/repository';
+import {del, getModelSchemaRef, post, requestBody} from '@loopback/rest';
+import {ValidateCurrencyInterceptor} from '../interceptors';
+import {UserCurrency} from '../models';
+import {UserCurrencyRepository} from '../repositories';
+// import { authenticate } from '@loopback/authentication';
+
+// @authenticate("jwt")
+export class UserCurrencyController {
+  constructor(
+    @repository(UserCurrencyRepository)
+    protected userCurrencyRepository: UserCurrencyRepository,
+  ) {}
+
+  @intercept(ValidateCurrencyInterceptor.BINDING_KEY)
+  @post('/user-currencies', {
+    responses: {
+      '200': {
+        description: 'create a UserCurrency model instance',
+        content: {'application/json': {schema: getModelSchemaRef(UserCurrency)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(UserCurrency, {
+            title: 'NewUserCurrency',
+          }),
+        },
+      },
+    })
+    userCurrency: UserCurrency,
+  ): Promise<UserCurrency> {
+    return this.userCurrencyRepository.create(userCurrency);
+  }
+
+  @del('/user-currencies', {
+    responses: {
+      '200': {
+        description: 'User.Cryptocurrency DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async delete(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(UserCurrency, {
+            title: 'NewUserCurrency',
+          }),
+        },
+      },
+    })
+    userCurrency: UserCurrency,
+  ): Promise<Count> {
+    return this.userCurrencyRepository.deleteAll({
+      userId: userCurrency.userId,
+      currencyId: userCurrency.currencyId,
+    });
+  }
+}
