feb5a80a7141abf62ac670a5abe182fa2d4fec64
abdulhakim2902 <hakim@blocksphere.id>
2021-08-07 11:27:26 +0700

    validate-currency.interceptor.ts: add new interceptor for curency validation

---
+++ src/interceptors/validate-currency.interceptor.ts
@@ -0,0 +1,88 @@
+import {
+  injectable,
+  Interceptor,
+  InvocationContext,
+  InvocationResult,
+  Provider,
+  ValueOrPromise,
+} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {HttpErrors} from '@loopback/rest';
+import {ControllerType} from '../enums';
+import {CurrencyRepository, UserCurrencyRepository} from '../repositories';
+
+/**
+ * This class will be bound to the application as an `Interceptor` during
+ * `boot`
+ */
+@injectable({tags: {key: ValidateCurrencyInterceptor.BINDING_KEY}})
+export class ValidateCurrencyInterceptor implements Provider<Interceptor> {
+  static readonly BINDING_KEY = `interceptors.${ValidateCurrencyInterceptor.name}`;
+
+  constructor(
+    @repository(CurrencyRepository)
+    protected currencyRepository: CurrencyRepository,
+    @repository(UserCurrencyRepository)
+    protected userCurrencyRepository: UserCurrencyRepository,
+  ) {}
+
+  /**
+   * This method is used by LoopBack context to produce an interceptor function
+   * for the binding.
+   *
+   * @returns An interceptor function
+   */
+  value() {
+    return this.intercept.bind(this);
+  }
+
+  /**
+   * The logic to intercept an invocation
+   * @param invocationCtx - Invocation context
+   * @param next - A function to invoke next interceptor or the target method
+   */
+  async intercept(invocationCtx: InvocationContext, next: () => ValueOrPromise<InvocationResult>) {
+    const className = invocationCtx.targetClass.name as ControllerType;
+
+    switch (className) {
+      case ControllerType.USERCURRENCY: {
+        invocationCtx.args[0].currencyId = invocationCtx.args[0].currencyId.toUpperCase();
+
+        const {userId, currencyId} = invocationCtx.args[0];
+
+        await this.currencyRepository.findById(currencyId);
+
+        // Check if user already has the crypto
+        const userCurrency = await this.userCurrencyRepository.findOne({
+          where: {
+            userId,
+            currencyId,
+          },
+        });
+
+        if (userCurrency) {
+          throw new HttpErrors.UnprocessableEntity('You already have this token');
+        }
+
+        break;
+      }
+
+      case ControllerType.CURRENCY: {
+        invocationCtx.args[0].id = invocationCtx.args[0].id.toUpperCase();
+
+        const currency = await this.currencyRepository.findOne({
+          where: {id: invocationCtx.args[0].id},
+        });
+
+        if (currency) throw new HttpErrors.UnprocessableEntity('Currency already exists!');
+
+        break;
+      }
+    }
+
+    // Add pre-invocation logic here
+    const result = await next();
+    // Add post-invocation logic here
+    return result;
+  }
+}
