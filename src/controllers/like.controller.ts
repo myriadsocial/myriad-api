63d132ff4c5ea2b849a66a8ff23e5594f99b7102
abdulhakim2902 <hakim@blocksphere.id>
2021-08-08 09:27:04 +0700

    rename to general naming

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,21 +1,20 @@
 import {intercept} from '@loopback/core';
-import {Count, CountSchema, repository} from '@loopback/repository';
+import {repository} from '@loopback/repository';
 import {del, getModelSchemaRef, param, post, requestBody} from '@loopback/rest';
-import {LikeType} from '../enums';
-import {ValidateLikePostInterceptor} from '../interceptors';
-import {Like, Post} from '../models';
+import {ValidateLikeInterceptor} from '../interceptors';
+import {Like} from '../models';
 import {LikeRepository} from '../repositories';
 // import {authenticate} from '@loopback/authentication';
 
 // @authenticate("jwt")
-export class PostLikeController {
+export class LikeController {
   constructor(
     @repository(LikeRepository)
     protected likeRepository: LikeRepository,
   ) {}
 
-  @intercept(ValidateLikePostInterceptor.BINDING_KEY)
-  @post('/posts/{id}/likes', {
+  @intercept(ValidateLikeInterceptor.BINDING_KEY)
+  @post('/likes', {
     responses: {
       '200': {
         description: 'Like model instance',
@@ -24,35 +23,29 @@ export class PostLikeController {
     },
   })
   async createLike(
-    @param.path.string('id') id: typeof Post.prototype.id,
     @requestBody({
       content: {
         'application/json': {
           schema: getModelSchemaRef(Like, {
             title: 'NewLike',
-            exclude: ['referenceId', 'type', 'state'],
+            exclude: ['state'],
           }),
         },
       },
     })
     like: Omit<Like, 'id'>,
   ): Promise<Like> {
-    like.referenceId = id;
     return this.likeRepository.create(like);
   }
 
-  @del('post/{postId}/likes/{likeId}', {
+  @del('/likes/{id}', {
     responses: {
       '200': {
         description: 'Like DELETE success',
-        content: {'application/json': {schema: CountSchema}},
       },
     },
   })
-  async delete(
-    @param.path.string('postId') postId: string,
-    @param.path.string('likeId') likeId: string,
-  ): Promise<Count> {
-    return this.likeRepository.deleteAll({type: LikeType.POST, referenceId: postId, id: likeId});
+  async delete(@param.path.string('id') id: string): Promise<void> {
+    return this.likeRepository.deleteById(id);
   }
 }
