55d58b2729e37e091099a2a7b7e28a7f0520f38c
abdulhakim2902 <hakim@blocksphere.id>
2021-08-09 10:59:50 +0700

    refactor polkadotJs Utils

--- src/helpers/polkadotJs-utils.ts
+++ src/helpers/polkadotJs-utils.ts
@@ -5,15 +5,9 @@ import {mnemonicGenerate} from '@polkadot/util-crypto';
 import {KeypairType} from '@polkadot/util-crypto/types';
 
 export class PolkadotJs {
-  provider: string;
-
-  constructor(wssProvider?: string) {
-    this.provider = wssProvider ?? '';
-  }
-
-  async polkadotApi(): Promise<ApiPromise> {
+  async polkadotApi(wssProvider: string): Promise<ApiPromise> {
     try {
-      const provider = new WsProvider(this.provider);
+      const provider = new WsProvider(wssProvider);
       const api = await new ApiPromise({provider}).isReadyOrError;
 
       return api;
@@ -22,9 +16,9 @@ export class PolkadotJs {
     }
   }
 
-  getKeyring(type?: string, addressFormat?: number): Keyring {
+  getKeyring(type = 'sr25519', addressFormat = 42): Keyring {
     return new Keyring({
-      type: type as KeypairType | '' as KeypairType,
+      type: type as KeypairType,
       ss58Format: addressFormat,
     });
   }
