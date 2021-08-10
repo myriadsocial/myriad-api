76ae978c73e77564c0a845277dc270149bd7ad83
abdulhakim2902 <hakim@blocksphere.id>
2021-08-10 19:58:58 +0700

    fixed application.ts

--- src/application.ts
+++ src/application.ts
@@ -1,7 +1,7 @@
 import {AuthenticationComponent} from '@loopback/authentication';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
-import {RepositoryMixin} from '@loopback/repository';
+import {RepositoryMixin, SchemaMigrationOptions} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {RestExplorerBindings, RestExplorerComponent} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
@@ -9,6 +9,23 @@ import dotenv from 'dotenv';
 import * as firebaseAdmin from 'firebase-admin';
 import path from 'path';
 import {JWTAuthenticationComponent} from './components';
+import currencySeed from './data-seed/currencies.json';
+import peopleSeed from './data-seed/people.json';
+import postSeed from './data-seed/posts.json';
+import userSeed from './data-seed/users.json';
+import {ExtendedPost} from './interfaces';
+import {InitDatabase} from './migrations/init-database';
+import {Currency, People, User} from './models';
+import {
+  CurrencyRepository,
+  ExperienceRepository,
+  PeopleRepository,
+  PostRepository,
+  TagRepository,
+  UserCurrencyRepository,
+  UserExperienceRepository,
+  UserRepository,
+} from './repositories';
 import {MySequence} from './sequence';
 import {
   CurrencyService,
@@ -68,7 +85,7 @@ export class MyriadApiApplication extends BootMixin(
   bindComponent() {
     this.component(RestExplorerComponent);
     this.component(AuthenticationComponent);
-    this.component(JWTAuthenticationComponent); // Mount jwt component
+    this.component(JWTAuthenticationComponent);
   }
 
   bindService() {
@@ -102,5 +119,38 @@ export class MyriadApiApplication extends BootMixin(
         }),
       });
     }
+  }
+
+  async migrateSchema(options?: SchemaMigrationOptions) {
+    await super.migrateSchema(options);
+
+    const init = await this.setInitDatabase();
+
+    await init.createUsers(userSeed as User[]);
+    await init.createCurrencies(currencySeed as Currency[]);
+    await init.createPeople(peopleSeed as People[]);
+    await init.createPost(postSeed as ExtendedPost[]);
+  }
+
+  async setInitDatabase(): Promise<InitDatabase> {
+    const currencyRepository = await this.getRepository(CurrencyRepository);
+    const experienceRepository = await this.getRepository(ExperienceRepository);
+    const peopleRepository = await this.getRepository(PeopleRepository);
+    const postRepository = await this.getRepository(PostRepository);
+    const tagRepository = await this.getRepository(TagRepository);
+    const userCurrencyRepository = await this.getRepository(UserCurrencyRepository);
+    const userExperienceRepository = await this.getRepository(UserExperienceRepository);
+    const userRepository = await this.getRepository(UserRepository);
+
+    return new InitDatabase(
+      userRepository,
+      currencyRepository,
+      peopleRepository,
+      postRepository,
+      userCurrencyRepository,
+      tagRepository,
+      experienceRepository,
+      userExperienceRepository,
+    );
   }
 }
