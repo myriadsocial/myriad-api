ce69817392aeb1b71560bcc66897dc407e0371a3
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-08-15 22:59:40 +0700

    changed default jwt secret

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   MONGO_DATABASE: process.env.MONGO_DATABASE,
-  TOKEN_SECRET_KEY: process.env.TOKEN_SECRET_KEY ?? '',
+  TOKEN_SECRET_KEY: process.env.TOKEN_SECRET_KEY ?? 'tok3n',
   TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN ?? '36000',
   REFRESH_SECRET_KEY: process.env.REFRESH_TOKEN_SECRET_KEY ?? 'r3fr35htok3n',
   REFRESH_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN ?? '216000',
