1c038ab5af8260de50dc4b9536dcb2c35a93364d
abdulhakim2902 <hakim@blocksphere.id>
2021-08-17 10:24:33 +0700

    post.walletaddress.controller.ts: fixed get post walletaddress if post created by user myriad

--- src/controllers/post-walletaddress.controller.ts
+++ src/controllers/post-walletaddress.controller.ts
@@ -1,5 +1,6 @@
 import {repository} from '@loopback/repository';
 import {get, getModelSchemaRef, HttpErrors, param, response} from '@loopback/rest';
+import {PlatformType} from '../enums';
 import {Wallet} from '../models';
 import {PeopleRepository, PostRepository} from '../repositories';
 
@@ -22,11 +23,22 @@ export class PostWalletAddress {
   })
   async getWalletAddress(@param.path.string('id') id: string): Promise<Wallet> {
     const post = await this.postRepository.findById(id);
+
+    const wallet = new Wallet();
+
+    if (!post.peopleId) {
+      if (post.platform === PlatformType.MYRIAD) {
+        wallet.walletAddress = post.createdBy;
+
+        return wallet;
+      } else {
+        throw new HttpErrors.NotFound('Walletaddress Not Found!');
+      }
+    }
+
     const people = await this.peopleRepository.findById(post.peopleId, {
       include: ['userSocialMedia'],
     });
-
-    const wallet = new Wallet();
 
     if (people.userSocialMedia) {
       wallet.walletAddress = people.userSocialMedia.userId;
