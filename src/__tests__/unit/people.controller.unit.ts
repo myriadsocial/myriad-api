2c21599261bd40ee285d0d15da054c90dbca657f
abdulhakim2902 <hakim@blocksphere.id>
2021-08-19 20:57:39 +0700

    people controller unit test

---
+++ src/__tests__/unit/people.controller.unit.ts
@@ -0,0 +1,85 @@
+import {
+  createStubInstance,
+  expect,
+  sinon,
+  StubbedInstanceWithSinonAccessor,
+  toJSON,
+} from '@loopback/testlab';
+import {PeopleController} from '../../controllers';
+import {CustomFilter, People} from '../../models';
+import {PeopleRepository} from '../../repositories';
+import {givenPeople} from '../helpers';
+
+describe('PeopleController', () => {
+  let peopleRepository: StubbedInstanceWithSinonAccessor<PeopleRepository>;
+  let controller: PeopleController;
+  let aPeopleWithId: People;
+  let aListOfPeople: People[];
+
+  beforeEach(resetRepositories);
+
+  describe('findPeopleById', () => {
+    it('returns a people if it exists', async () => {
+      const findById = peopleRepository.stubs.findById;
+      findById.resolves(aPeopleWithId);
+      expect(await controller.findById(aPeopleWithId.id as string)).to.eql(aPeopleWithId);
+      sinon.assert.calledWith(findById, aPeopleWithId.id);
+    });
+  });
+
+  describe('findPeople', () => {
+    it('returns multiple people if they exist', async () => {
+      const find = peopleRepository.stubs.find;
+      find.resolves(aListOfPeople);
+      expect(await controller.find()).to.eql(aListOfPeople);
+      sinon.assert.called(find);
+    });
+
+    it('returns empty list if no people exist', async () => {
+      const find = peopleRepository.stubs.find;
+      const expected: People[] = [];
+      find.resolves(expected);
+      expect(await controller.find()).to.eql(expected);
+      sinon.assert.called(find);
+    });
+
+    it('users the provided filter', async () => {
+      const find = peopleRepository.stubs.find;
+      const filter = toJSON({where: {username: 'elonmusk'}});
+
+      find.resolves(aListOfPeople);
+      await controller.find(filter as CustomFilter);
+      sinon.assert.calledWith(find, filter);
+    });
+  });
+
+  describe('deletePeople', () => {
+    it('successfully deletes existing items', async () => {
+      const deleteById = peopleRepository.stubs.deleteById;
+      deleteById.resolves();
+      await controller.deleteById(aPeopleWithId.id as string);
+      sinon.assert.calledWith(deleteById, aPeopleWithId.id);
+    });
+  });
+
+  function resetRepositories() {
+    peopleRepository = createStubInstance(PeopleRepository);
+    aPeopleWithId = givenPeople({
+      id: '1',
+    });
+    aListOfPeople = [
+      aPeopleWithId,
+      givenPeople({
+        id: '2',
+        name: 'Gavin Wood',
+        username: 'gavofyork',
+        platform: 'twitter',
+        originUserId: '33962758',
+        profilePictureURL:
+          'https://pbs.twimg.com/profile_images/981390758870683656/RxA_8cyN_400x400.jpg',
+      }),
+    ] as People[];
+
+    controller = new PeopleController(peopleRepository);
+  }
+});
