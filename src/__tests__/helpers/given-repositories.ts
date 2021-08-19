3d37e0e32dbcb2863cead6ce2204d9d5fde97e29
abdulhakim2902 <hakim@blocksphere.id>
2021-08-19 21:00:59 +0700

    set given repositories

---
+++ src/__tests__/helpers/given-repositories.ts
@@ -0,0 +1,38 @@
+import {MyriadApiApplication} from '../../application';
+import {
+  CommentRepository,
+  CurrencyRepository,
+  FriendRepository,
+  LikeRepository,
+  PeopleRepository,
+  PostRepository,
+  UserRepository,
+} from '../../repositories';
+
+export async function givenUserRepository(app: MyriadApiApplication) {
+  return app.getRepository(UserRepository);
+}
+
+export async function givenFriendRepository(app: MyriadApiApplication) {
+  return app.getRepository(FriendRepository);
+}
+
+export async function givenCurrencyRepository(app: MyriadApiApplication) {
+  return app.getRepository(CurrencyRepository);
+}
+
+export async function givenPeopleRepository(app: MyriadApiApplication) {
+  return app.getRepository(PeopleRepository);
+}
+
+export async function givenCommentRepository(app: MyriadApiApplication) {
+  return app.getRepository(CommentRepository);
+}
+
+export async function givenPostRepository(app: MyriadApiApplication) {
+  return app.getRepository(PostRepository);
+}
+
+export async function givenLikeRepository(app: MyriadApiApplication) {
+  return app.getRepository(LikeRepository);
+}
