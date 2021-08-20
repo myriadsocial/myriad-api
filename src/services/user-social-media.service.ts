480dc97d41983b22661aeba072f51187ed0ee5f4
abdulhakim2902 <hakim@blocksphere.id>
2021-08-21 01:38:05 +0700

    user-social-media.service.ts: refactor

--- src/services/user-social-media.service.ts
+++ src/services/user-social-media.service.ts
@@ -17,87 +17,85 @@ export class UserSocialMediaService {
   async createSocialMedia(people: ExtendedPeople): Promise<UserSocialMedia> {
     const {name, originUserId, username, platform, profilePictureURL, publicKey} = people;
 
-    // Verify SocialMedia
+    const foundPeople = await this.peopleRepository.findOne({
+      where: {
+        originUserId: originUserId,
+        platform: platform,
+      },
+    });
+
+    if (!foundPeople) {
+      const newPeople = await this.peopleRepository.create({
+        name,
+        username,
+        originUserId,
+        platform,
+        profilePictureURL,
+        createdAt: new Date().toString(),
+        updatedAt: new Date().toString(),
+      });
+
+      const {getKeyring, getHexPublicKey} = new PolkadotJs();
+      const newKey = getKeyring().addFromUri('//' + newPeople.id);
+
+      this.peopleRepository.updateById(newPeople.id, {
+        walletAddress: getHexPublicKey(newKey),
+      }) as Promise<void>;
+
+      return this.peopleRepository.userSocialMedia(newPeople.id).create({
+        userId: publicKey,
+        platform: platform as PlatformType,
+        verified: true,
+        createdAt: new Date().toString(),
+        updatedAt: new Date().toString(),
+      });
+    }
+
     const userSocialMedia = await this.userSocialMediaRepository.findOne({
+      where: {
+        peopleId: people.id,
+        platform: platform as PlatformType,
+      },
+    });
+
+    if (userSocialMedia) {
+      if (userSocialMedia.userId !== publicKey) {
+        throw new HttpErrors.UnprocessableEntity(`Another user already claim this ${platform}!`);
+      }
+
+      if (userSocialMedia.verified) {
+        throw new HttpErrors.UnprocessableEntity('You already verified this social media');
+      }
+
+      userSocialMedia.verified = true;
+      userSocialMedia.updatedAt = new Date().toString();
+
+      this.userSocialMediaRepository.updateById(
+        userSocialMedia.id,
+        userSocialMedia,
+      ) as Promise<void>;
+
+      return userSocialMedia;
+    }
+
+    const result = await this.userSocialMediaRepository.findOne({
       where: {
         userId: publicKey,
         platform: platform as PlatformType,
       },
     });
 
-    if (userSocialMedia) {
-      const person = await this.peopleRepository.findOne({
-        where: {
-          id: userSocialMedia.peopleId,
-        },
-      });
+    if (result)
+      throw new HttpErrors.UnprocessableEntity(
+        `You already claimed another ${platform}. If you want to change to another ${platform}. Please disconnect then reconnect again!`,
+      );
 
-      if (person && person.originUserId !== originUserId) {
-        throw new HttpErrors.NotFound(`This ${person.platform} does not belong to you!`);
-      }
-    }
-
-    const foundPeople = await this.peopleRepository.findOne({
-      where: {originUserId, platform},
-    });
-
-    if (foundPeople) {
-      const peopleSocialMedia = await this.userSocialMediaRepository.findOne({
-        where: {
-          peopleId: people.id,
-          platform: platform as PlatformType,
-        },
-      });
-
-      if (!peopleSocialMedia) {
-        return this.peopleRepository.userSocialMedia(foundPeople.id).create({
-          userId: publicKey,
-          platform: platform as PlatformType,
-          verified: true,
-          createdAt: new Date().toString(),
-          updatedAt: new Date().toString(),
-        });
-      }
-
-      if (peopleSocialMedia.userId === people.publicKey) {
-        if (peopleSocialMedia.verified) {
-          throw new HttpErrors.UnprocessableEntity('You already verified this social media');
-        }
-
-        peopleSocialMedia.verified = true;
-        peopleSocialMedia.updatedAt = new Date().toString();
-        this.userSocialMediaRepository.updateById(
-          peopleSocialMedia.id,
-          peopleSocialMedia,
-        ) as Promise<void>;
-
-        return peopleSocialMedia;
-      }
-
-      throw new HttpErrors.NotFound('This social media has been claimed by other user');
-    }
-
-    const newPeople = await this.peopleRepository.create({
-      name,
-      username,
-      originUserId,
-      platform,
-      profilePictureURL,
-      createdAt: new Date().toString(),
-      updatedAt: new Date().toString(),
-    });
-
-    const {getKeyring, getHexPublicKey} = new PolkadotJs();
-    const newKey = getKeyring().addFromUri('//' + newPeople.id);
-
-    this.peopleRepository.updateById(newPeople.id, {
-      walletAddress: getHexPublicKey(newKey),
-    }) as Promise<void>;
-
-    return this.peopleRepository.userSocialMedia(newPeople.id).create({
+    return this.peopleRepository.userSocialMedia(foundPeople.id).create({
       userId: publicKey,
       platform: platform as PlatformType,
       verified: true,
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
     });
   }
 }
