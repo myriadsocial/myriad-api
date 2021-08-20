4cc2c30656f32ee6d0ac04a01d8ec6b2038da1f2
abdulhakim2902 <hakim@blocksphere.id>
2021-08-20 09:16:15 +0700

    notification controller unit test

---
+++ src/__tests__/unit/notification.controller.unit.ts
@@ -0,0 +1,104 @@
+import {
+  createStubInstance,
+  expect,
+  sinon,
+  StubbedInstanceWithSinonAccessor,
+  toJSON,
+} from '@loopback/testlab';
+import {NotificationController} from '../../controllers';
+import {NotificationType} from '../../enums';
+import {CustomFilter, Notification} from '../../models';
+import {NotificationRepository} from '../../repositories';
+import {givenNotification} from '../helpers';
+
+describe('NotificationController', () => {
+  let notificationRepository: StubbedInstanceWithSinonAccessor<NotificationRepository>;
+  let controller: NotificationController;
+  let aNotificationWithId: Notification;
+  let aChangedNotification: Notification;
+  let aListOfNotifications: Notification[];
+
+  beforeEach(resetRepositories);
+
+  describe('findNotificationById', () => {
+    it('returns a notification if it exists', async () => {
+      const findById = notificationRepository.stubs.findById;
+      findById.resolves(aNotificationWithId);
+      expect(await controller.findById(aNotificationWithId.id as string)).to.eql(
+        aNotificationWithId,
+      );
+      sinon.assert.calledWith(findById, aNotificationWithId.id);
+    });
+  });
+
+  describe('findNotifications', () => {
+    it('returns multiple notifications if they exist', async () => {
+      const find = notificationRepository.stubs.find;
+      find.resolves(aListOfNotifications);
+      expect(await controller.find()).to.eql(aListOfNotifications);
+      sinon.assert.called(find);
+    });
+
+    it('returns empty list if no notifications exist', async () => {
+      const find = notificationRepository.stubs.find;
+      const expected: Notification[] = [];
+      find.resolves(expected);
+      expect(await controller.find()).to.eql(expected);
+      sinon.assert.called(find);
+    });
+
+    it('uses the provided filter', async () => {
+      const find = notificationRepository.stubs.find;
+      const filter = toJSON({
+        where: {from: '0x06cc7ed22ebd12ccc28fb9c0d14a5c4420a331d89a5fef48b915e8449ee61866'},
+      });
+
+      find.resolves(aListOfNotifications);
+      await controller.find(filter as CustomFilter);
+      sinon.assert.calledWith(find, filter);
+    });
+  });
+
+  describe('updateNotification', () => {
+    it('successfully updates existing items', async () => {
+      const updateById = notificationRepository.stubs.updateById;
+      updateById.resolves();
+      await controller.updateById(aNotificationWithId.id as string, aChangedNotification);
+      sinon.assert.calledWith(updateById, aNotificationWithId.id, aChangedNotification);
+    });
+  });
+
+  describe('deleteNotification', () => {
+    it('successfully deletes existing items', async () => {
+      const deleteById = notificationRepository.stubs.deleteById;
+      deleteById.resolves();
+      await controller.deleteById(aNotificationWithId.id as string);
+      sinon.assert.calledWith(deleteById, aNotificationWithId.id);
+    });
+  });
+
+  function resetRepositories() {
+    notificationRepository = createStubInstance(NotificationRepository);
+    aNotificationWithId = givenNotification({
+      id: '1',
+    });
+    aListOfNotifications = [
+      aNotificationWithId,
+      givenNotification({
+        id: '261',
+        type: NotificationType.FRIEND_REQUEST,
+        read: false,
+        message: 'sent you friend request',
+        referenceId: '0x06cc7ed22ebd12ccc28fb9c0d14a5c4420a331d89a5fef48b915e8449ee61865',
+        from: '0x06cc7ed22ebd12ccc28fb9c0d14a5c4420a331d89a5fef48b915e8449ee61866',
+        to: '0x06cc7ed22ebd12ccc28fb9c0d14a5c4420a331d89a5fef48b915e8449ee61865',
+      }),
+    ] as Notification[];
+    aChangedNotification = givenNotification({
+      id: aNotificationWithId.id,
+      message: 'accept your friend request',
+    });
+
+    controller = new NotificationController(notificationRepository);
+  }
+});
