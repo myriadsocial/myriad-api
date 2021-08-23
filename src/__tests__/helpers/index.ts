6b5468ac2b821cb5d991cc3c323c0192cee8e9ac
abdulhakim2902 <hakim@blocksphere.id>
2021-08-23 19:00:41 +0700

    set init database for integration test

--- src/__tests__/helpers/index.ts
+++ src/__tests__/helpers/index.ts
@@ -1,3 +1,5 @@
+export * from './database.helper';
 export * from './given-instances';
 export * from './given-repositories';
 export * from './setup-application';
+export * from './testdb.datascource';
