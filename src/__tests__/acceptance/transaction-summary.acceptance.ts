81bdd5efbc410dd778c3b47eb458f07151073b64
abdulhakim2902 <hakim@blocksphere.id>
2021-08-23 11:41:45 +0700

    renamed and add new test for transaction history acceptance test

--- src/__tests__/acceptance/transaction-summary.acceptance.ts
+++ src/__tests__/acceptance/transaction-summary.acceptance.ts
@@ -1,6 +1,6 @@
 import {Client, expect} from '@loopback/testlab';
 import {MyriadApiApplication} from '../../application';
-import {DefaultCurrencyType} from '../../enums';
+import {DefaultCurrencyType, TransactionType} from '../../enums';
 import {Transaction, User} from '../../models';
 import {TransactionRepository, UserRepository} from '../../repositories';
 import {
@@ -12,7 +12,7 @@ import {
 } from '../helpers';
 
 /* eslint-disable  @typescript-eslint/no-invalid-this */
-describe('TransactionHistoryApplication', function () {
+describe('TransactionSummaryApplication', function () {
   this.timeout(10000);
   let app: MyriadApiApplication;
   let client: Client;
@@ -22,6 +22,8 @@ describe('TransactionHistoryApplication', function () {
   let transactionReceivedInstance1: Transaction;
   let transactionSentInstance2: Transaction;
   let transactionReceivedInstance2: Transaction;
+  let transactionSentInstance3: Transaction;
+  let transactionReceivedInstance3: Transaction;
   let user: User;
 
   before(async () => {
@@ -44,46 +46,73 @@ describe('TransactionHistoryApplication', function () {
     user = await givenUserInstance(userRepository);
     transactionSentInstance1 = await givenTransactionInstance(transactionRepository, {
       to: user.id,
-      postId: '1',
+      referenceId: '1',
+      type: TransactionType.POST,
     });
     transactionReceivedInstance1 = await givenTransactionInstance(transactionRepository, {
       from: user.id,
     });
     transactionSentInstance2 = await givenTransactionInstance(transactionRepository, {
       to: user.id,
-      postId: '1',
+      referenceId: '1',
+      type: TransactionType.POST,
     });
     transactionReceivedInstance2 = await givenTransactionInstance(transactionRepository, {
       from: user.id,
     });
+    transactionSentInstance3 = await givenTransactionInstance(transactionRepository, {
+      to: user.id,
+      referenceId: '2',
+      type: TransactionType.COMMENT,
+    });
+    transactionReceivedInstance3 = await givenTransactionInstance(transactionRepository, {
+      from: user.id,
+    });
   });
 
-  it('gets a user transaction history', async function () {
-    const response = await client.get(`/users/${user.id}/transaction-histories`).send().expect(200);
+  it('gets a user transaction summary', async function () {
+    const response = await client.get(`/users/${user.id}/transaction-summary`).send().expect(200);
 
     expect(response.body).to.deepEqual({
       sent: [
         {
           currencyId: DefaultCurrencyType.AUSD,
-          amount: transactionSentInstance1.amount + transactionSentInstance2.amount,
+          amount:
+            transactionSentInstance1.amount +
+            transactionSentInstance2.amount +
+            transactionSentInstance3.amount,
         },
       ],
       received: [
         {
           currencyId: DefaultCurrencyType.AUSD,
-          amount: transactionReceivedInstance1.amount + transactionReceivedInstance2.amount,
+          amount:
+            transactionReceivedInstance1.amount +
+            transactionReceivedInstance2.amount +
+            transactionReceivedInstance3.amount,
         },
       ],
     });
   });
 
-  it('gets a post transaction history', async function () {
-    const response = await client.get(`/posts/1/transaction-histories`).send().expect(200);
+  it('gets a post transaction summary', async function () {
+    const response = await client.get(`/posts/1/transaction-summary`).send().expect(200);
 
     expect(response.body).to.containDeep([
       {
         currencyId: DefaultCurrencyType.AUSD,
         amount: transactionSentInstance1.amount + transactionSentInstance2.amount,
+      },
+    ]);
+  });
+
+  it('gets a comment transaction summary', async function () {
+    const response = await client.get(`/comments/2/transaction-summary`).send().expect(200);
+
+    expect(response.body).to.containDeep([
+      {
+        currencyId: DefaultCurrencyType.AUSD,
+        amount: transactionSentInstance3.amount,
       },
     ]);
   });
