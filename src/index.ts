8d7c6fb378ce7409201c962e91a4e1bdad656d7e
abdulhakim2902 <hakim@blocksphere.id>
2021-08-25 19:32:21 +0700

    reset queue before app start

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './application';
 export async function main(options: ApplicationConfig = {}) {
   const app = new MyriadApiApplication(options);
   await app.boot();
+  await app.migrateSchema();
   await app.start();
 
   const url = app.restServer.url;
