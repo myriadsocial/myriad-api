299972b538f92fff140bc3be02b1f6457602cec2
abdulhakim2902 <hakim@blocksphere.id>
2021-08-27 13:42:41 +0700

    url.utils.ts: new getOpenGraph method

--- src/utils/url.utils.ts
+++ src/utils/url.utils.ts
@@ -1,13 +1,21 @@
+import ogs from 'open-graph-scraper';
 import {PlatformType} from '../enums';
+import {EmbeddedURL, Media} from '../models';
 
 export class UrlUtils {
   detail: string[];
 
-  constructor(socialMediaURL: string) {
-    this.detail = socialMediaURL
-      .replace(/(https?:\/\/)?(www.)?/i, '')
-      .replace(new RegExp(/user\/|u\/|r\//), '')
-      .split('/');
+  constructor(socialMediaURL?: string) {
+    this.detail = this.getDetail(socialMediaURL ?? '');
+  }
+
+  getDetail(socialMediaURL: string): string[] {
+    return socialMediaURL
+      ? socialMediaURL
+          .replace(/(https?:\/\/)?(www.)?/i, '')
+          .replace(new RegExp(/user\/|u\/|r\//), '')
+          .split('/')
+      : [];
   }
 
   getPlatform(): PlatformType {
@@ -20,5 +28,28 @@ export class UrlUtils {
 
   getUsername(): string {
     return this.detail[1];
+  }
+
+  async getOpenGraph(url: string): Promise<EmbeddedURL> {
+    const {result} = await ogs({url});
+    const embeddedURL = new EmbeddedURL();
+    const embedded: any = result;
+
+    embeddedURL.description = embedded.ogDescription ?? '';
+    embeddedURL.title = embedded.ogTitle ?? '';
+    embeddedURL.siteName = embedded.ogSiteName ?? '';
+    embeddedURL.url = embedded.ogUrl ?? '';
+
+    if (embedded.ogImage) {
+      delete embedded.ogImage.type;
+      embeddedURL.image = new Media(embedded.ogImage);
+    }
+
+    if (embedded.ogVideo) {
+      delete embedded.ogVideo.type;
+      embeddedURL.video = new Media(embedded.ogVideo);
+    }
+
+    return embeddedURL;
   }
 }
