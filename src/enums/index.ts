bee52660c6521c3f4f106a240a5131d2c275ac82
abdulhakim2902 <hakim@blocksphere.id>
2021-08-31 13:29:44 +0700

    log.type.ts: added activity log constant

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -3,6 +3,7 @@ export * from './content-type.enum';
 export * from './default-currency-type.enum';
 export * from './experience-status-type.enum';
 export * from './friendstatus-type.enum';
+export * from './log-type.enum';
 export * from './notification-type.enum';
 export * from './platform-type.enum';
 export * from './timelinefilter-type.enum';
