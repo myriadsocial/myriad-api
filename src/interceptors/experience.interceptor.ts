ee425f48c7334da11e3d300f0c5e5918b59d25e0
abdulhakim2902 <hakim@blocksphere.id>
2021-09-01 20:59:30 +0700

    experience.interceptor.ts: refactor

--- src/interceptors/experience.interceptor.ts
+++ src/interceptors/experience.interceptor.ts
@@ -4,17 +4,11 @@ import {
   InvocationContext,
   InvocationResult,
   Provider,
-  service,
   ValueOrPromise,
 } from '@loopback/core';
-import {Count, repository} from '@loopback/repository';
+import {repository} from '@loopback/repository';
 import {HttpErrors} from '@loopback/rest';
-import * as _ from 'lodash';
-import {MethodType, StatusType} from '../enums';
 import {ExperienceRepository, UserExperienceRepository, UserRepository} from '../repositories';
-import {ExperienceService} from '../services';
-import {setStatus} from '../utils/filter-utils';
-
 /**
  * This class will be bound to the application as an `Interceptor` during
  * `boot`
@@ -30,8 +24,6 @@ export class ExperienceInterceptor implements Provider<Interceptor> {
     protected userExperienceRepository: UserExperienceRepository,
     @repository(UserRepository)
     protected userRepository: UserRepository,
-    @service(ExperienceService)
-    protected experienceService: ExperienceService,
   ) {}
 
   /**
@@ -51,131 +43,73 @@ export class ExperienceInterceptor implements Provider<Interceptor> {
    */
   async intercept(invocationCtx: InvocationContext, next: () => ValueOrPromise<InvocationResult>) {
     const methodName = invocationCtx.methodName;
+    const userId = invocationCtx.args[0];
+    const experienceId = invocationCtx.args[1];
 
-    let count = 0;
+    let numberOfUserExperience = 0;
 
     switch (methodName) {
-      case MethodType.CLONE: {
-        const {userId, experienceId} = invocationCtx.args[0];
-        const experience = await this.experienceRepository.findById(experienceId);
-        const user = await this.userExperienceRepository.count({userId});
-
-        if (user.count >= 10)
-          throw new HttpErrors.UnprocessableEntity('Experience must not exceed 10 experiences');
-        else count = user.count;
-
-        if (userId === experience.createdBy)
-          throw new HttpErrors.UnprocessableEntity('Experience already belong to you!');
-
-        experience.cloned = experience.cloned + 1;
-        experience.origin = false;
-        experience.clonedFrom = experience.id;
-
-        const newExperience = await this.experienceRepository.create(_.omit(experience, 'id'));
-
-        const result = await this.experienceRepository.count({
-          clonedFrom: experience.id,
-        });
-
-        this.experienceRepository.updateAll(
-          {cloned: result.count},
-          {
-            or: [
-              {
-                clonedFrom: experience.clonedFrom,
-              },
-              {
-                id: experience.id,
-              },
-            ],
-          },
-        ) as Promise<Count>;
-
-        invocationCtx.args[0] = {
-          userId: userId,
-          experienceId: newExperience.id,
-          clonedFrom: experience.clonedFrom,
-          createdAt: new Date().toString(),
-          updatedAt: new Date().toString(),
-        };
-
+      case 'create':
+        numberOfUserExperience = await this.validateNumberOfUserExperience(userId);
         break;
-      }
 
-      case MethodType.MODIFY: {
-        const newExperience = invocationCtx.args[1];
-        const experience = await this.experienceRepository.findById(invocationCtx.args[0]);
-        const user = await this.userRepository.findById(newExperience.createdBy);
-
-        if (experience.createdBy === user.id)
-          throw new HttpErrors.UnprocessableEntity('Experience already belong to you!');
-
-        newExperience.tags = setStatus(newExperience.tags, StatusType.NONE);
-        newExperience.people = setStatus(newExperience.people, StatusType.NONE);
-        newExperience.origin = true;
-        newExperience.clonedFrom = null;
-        newExperience.cloned = 0;
-
-        invocationCtx.args[1] = newExperience;
-
+      case 'clone':
+        numberOfUserExperience = await this.validateCloneExperience(userId, experienceId);
         break;
-      }
 
-      case MethodType.CREATENEW: {
-        const experience = invocationCtx.args[0];
-        const user = await this.userExperienceRepository.count({userId: experience.createdBy});
-
-        if (user.count >= 10)
-          throw new HttpErrors.UnprocessableEntity('Experience must not exceed 10 experiences');
-        else count = user.count;
-
-        experience.tags = setStatus(experience.tags, StatusType.NONE);
-        experience.people = setStatus(experience.people, StatusType.NONE);
-        experience.createdAt = new Date().toString();
-        experience.updatedAt = new Date().toString();
-
-        invocationCtx.args[0] = experience;
-
+      case 'modify':
+        invocationCtx.args[2] = Object.assign(invocationCtx.args[2], {createdBy: userId});
         break;
-      }
     }
+
     // Add pre-invocation logic here
     const result = await next();
     // Add post-invocation logic here
-    switch (methodName) {
-      case MethodType.CLONE: {
-        if (count === 0) {
-          await this.userRepository.updateById(result.userId, {onTimeline: result.experienceId});
-        }
 
-        break;
-      }
+    if ((methodName === 'create' || methodName === 'clone') && numberOfUserExperience === 0) {
+      await this.userRepository.updateById(userId, {
+        onTimeline: methodName === 'create' ? result.id : result.experienceId,
+      });
+    }
 
-      case MethodType.CREATENEW: {
-        if (count === 0) {
-          await this.userRepository.updateById(result.createdBy, {onTimeline: result.id});
-        }
+    if (methodName === 'clone' || methodName === 'modify') {
+      const {count: currentNumberOfUserExperience} = await this.userExperienceRepository.count({
+        experienceId,
+        cloned: true,
+      });
 
-        break;
-      }
-
-      case MethodType.MODIFY: {
-        this.userExperienceRepository.updateAll(
-          {clonedFrom: null},
-          {userId: invocationCtx.args[0], experienceId: invocationCtx.args[1]},
-        ) as Promise<Count>;
-
-        break;
-      }
-
-      case MethodType.UPDATEBYID: {
-        this.experienceService.updateOtherExperience(
-          invocationCtx.args[0],
-          invocationCtx.args[1],
-        ) as Promise<void>;
-      }
+      await this.experienceRepository.updateById(experienceId, {
+        clonedCount: currentNumberOfUserExperience,
+      });
     }
 
     return result;
+  }
+
+  async validateCloneExperience(userId: string, experienceId: string): Promise<number> {
+    // Check if experience not belong to user
+    const experience = await this.experienceRepository.findById(experienceId);
+
+    if (userId === experience.createdBy)
+      throw new HttpErrors.UnprocessableEntity('Experience already belong to you!');
+
+    // Check if user has been cloned this experience
+    const cloned = await this.userExperienceRepository.findOne({
+      where: {userId, experienceId, cloned: true},
+    });
+
+    if (cloned) throw new HttpErrors.UnprocessableEntity('You already cloned this experience!');
+
+    return this.validateNumberOfUserExperience(userId);
+  }
+
+  async validateNumberOfUserExperience(userId: string): Promise<number> {
+    // Check if user has experiences less than equal 10
+    const {count} = await this.userExperienceRepository.count({userId});
+
+    if (count >= 10)
+      throw new HttpErrors.UnprocessableEntity('Experience must not exceed 10 experiences');
+
+    return count;
   }
 }
