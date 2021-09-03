f77048834890905a3db1cf26eb6ce7c8da88d2f8
abdulhakim2902 <hakim@blocksphere.id>
2021-09-03 16:55:50 +0700

    comment-link.repository.ts: added new repository

---
+++ src/repositories/comment-link.repository.ts
@@ -0,0 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {CommentLink, CommentLinkRelations} from '../models';
+
+export class CommentLinkRepository extends DefaultCrudRepository<
+  CommentLink,
+  typeof CommentLink.prototype.id,
+  CommentLinkRelations
+> {
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource,
+  ) {
+    super(CommentLink, dataSource);
+  }
+}
