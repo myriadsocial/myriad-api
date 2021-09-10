bac67c50246a65e0d7905a57b7aa0e749c2e67e5
abdulhakim2902 <hakim@blocksphere.id>
2021-09-10 10:10:51 +0700

    fixed test

--- src/__tests__/acceptance/like.acceptance.ts
+++ src/__tests__/acceptance/like.acceptance.ts
@@ -43,34 +43,13 @@ describe('LikeApplication', function () {
           comments: 0,
           likes: 0,
           dislikes: 0,
+          debates: 0,
         },
       },
       true,
     );
     const post = postResponse.ops[0];
     const like = givenLike({referenceId: post._id.toString()});
-    const response = await client.post('/likes').send(like).expect(200);
-    expect(response.body).to.containDeep(like);
-    const result = await likeRepository.findById(response.body.id);
-    expect(result).to.containDeep(like);
-  });
-
-  it('updates a like if exists', async function () {
-    const postResponse = await givenPostInstance(
-      postRepository,
-      {
-        metric: {
-          comments: 0,
-          likes: 0,
-          dislikes: 0,
-        },
-      },
-      true,
-    );
-    const post = postResponse.ops[0];
-    await givenLikeInstance(likeRepository, {referenceId: post._id.toString()});
-
-    const like = givenLike({referenceId: post._id.toString(), state: false});
     const response = await client.post('/likes').send(like).expect(200);
     expect(response.body).to.containDeep(like);
     const result = await likeRepository.findById(response.body.id);
@@ -86,22 +65,20 @@ describe('LikeApplication', function () {
             comments: 0,
             likes: 0,
             dislikes: 0,
+            debates: 0,
           },
         },
         true,
       )
     ).ops[0];
-    const id = postResponse._id.toString();
-    delete postResponse._id;
-    const post = {
-      id,
-      ...postResponse,
-    };
+    const post = Object.assign(postResponse, {
+      id: postResponse._id.toString(),
+      _id: undefined,
+    });
 
     const like = givenLike({referenceId: post.id});
     const response = await client.post('/likes').send(like);
 
-    sleep(500);
     const resultPost = await postRepository.findById(response.body.referenceId);
     post.metric.likes = post.metric.likes + 1;
     expect(resultPost).to.containDeep(post);
@@ -115,6 +92,7 @@ describe('LikeApplication', function () {
           comments: 0,
           likes: 1,
           dislikes: 0,
+          debates: 0,
         },
       },
       true,
@@ -129,17 +107,8 @@ describe('LikeApplication', function () {
       EntityNotFoundError,
     );
 
-    sleep(500);
     const resultPost = await postRepository.findById(like.referenceId);
     post.metric.likes = post.metric.likes - 1;
     expect(resultPost.metric.likes).to.equal(post.metric.likes);
   });
 });
-
-function sleep(milliseconds: number) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
