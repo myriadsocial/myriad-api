9f0d58f71068ed37a13e3c28218761ebfe42696f
abdulhakim2902 <hakim@blocksphere.id>
2021-09-13 15:51:02 +0700

    moved ausd reward amount to env

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,8 @@ export const config = {
   MYRIAD_MNEMONIC: process.env.MYRIAD_FAUCET_MNEMONIC ?? '',
   MYRIAD_REWARD_AMOUNT: +(process.env.MYRIAD_REWARD_AMOUNT ?? 0),
 
+  AUSD_REWARD_AMOUNT: +(process.env.AUSD_REWARD_AMOUNT ?? 0),
+
   MONGO_HOST: process.env.MONGO_HOST,
   MONGO_PORT: parseInt(process.env.MONGO_PORT ?? ''),
   MONGO_USER: process.env.MONGO_USER,
