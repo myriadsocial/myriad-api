da608c4e176fce031fd8493fd7d86d81a871c5bb
abdulhakim2902 <hakim@blocksphere.id>
2021-09-30 14:31:19 +0700

    MYR-912, MYR-913, MYR-915: fixed prettier

--- src/services/friend.service.ts
+++ src/services/friend.service.ts
@@ -16,7 +16,10 @@ export class FriendService {
     public notificationService: NotificationService,
   ) {}
 
-  async validateFriendRequest(requesteeId: string, requestorId: string): Promise<void> {
+  async validateFriendRequest(
+    requesteeId: string,
+    requestorId: string,
+  ): Promise<void> {
     if (requesteeId === requestorId) {
       throw new HttpErrors.UnprocessableEntity('Cannot add itself');
     }
@@ -50,18 +53,26 @@ export class FriendService {
     if (friend) {
       switch (friend.status) {
         case FriendStatusType.APPROVED: {
-          throw new HttpErrors.UnprocessableEntity('You already friend with this user');
+          throw new HttpErrors.UnprocessableEntity(
+            'You already friend with this user',
+          );
         }
 
         case FriendStatusType.PENDING: {
           if (requestorId === friend.requestorId)
-            throw new HttpErrors.UnprocessableEntity("Please wait for your friend's approval!");
+            throw new HttpErrors.UnprocessableEntity(
+              "Please wait for your friend's approval!",
+            );
 
-          throw new HttpErrors.UnprocessableEntity('Your friend waited for your approval!');
+          throw new HttpErrors.UnprocessableEntity(
+            'Your friend waited for your approval!',
+          );
         }
 
         case FriendStatusType.BLOCKED: {
-          throw new HttpErrors.UnprocessableEntity('You cannot added this friend!');
+          throw new HttpErrors.UnprocessableEntity(
+            'You cannot added this friend!',
+          );
         }
       }
     }
@@ -128,7 +139,10 @@ export class FriendService {
   async deleteById(id: string): Promise<void> {
     const friend = await this.friendRepository.findById(id);
 
-    await this.notificationService.cancelFriendRequest(friend.requestorId, friend.requesteeId);
+    await this.notificationService.cancelFriendRequest(
+      friend.requestorId,
+      friend.requesteeId,
+    );
     await this.friendRepository.deleteById(id);
   }
 }
