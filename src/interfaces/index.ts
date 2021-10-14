e03da7fb66d52832cde39dff144f54405986a641
Reinaldo Gunawan <rei@kickavenue.com>
2021-10-14 16:12:02 +0700

    renamed post.interface.ts -> extended-post.interface.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,7 +4,7 @@ export * from './meta-pagination.interface';
 export * from './metric.interface';
 export * from './new-auth-request.interface';
 export * from './payment-info.interface';
-export * from './post.interface';
+export * from './extended-post.interface';
 export * from './refresh-grant.interface';
 export * from './tokenobject.inteface';
 export * from './transaction-summary.interface';
