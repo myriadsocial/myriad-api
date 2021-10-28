b564e106b525b6c9cf830fb58dd00ed0602a61d1
abdulhakim2902 <hakim@blocksphere.id>
2021-10-28 10:08:55 +0800

    MYR-1015: added new enum const

--- src/enums/report-status-type.enum.ts
+++ src/enums/report-status-type.enum.ts
@@ -1,5 +1,13 @@
 export enum ReportStatusType {
   PENDING = 'pending',
   REMOVED = 'removed',
-  IGNORE = 'ignored',
+  IGNORED = 'ignored',
+  APPROVED = 'approved',
+}
+
+export enum PenaltyStatusType {
+  BANNED = 'banned',
+  PENALTY1 = 'penalty 1',
+  PENALTY2 = 'penalty 2',
+  PENALTY3 = 'penalty 3',
 }
