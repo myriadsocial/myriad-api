430ff473028c6947d787c2a87783c2a376f55b1b
abdulhakim2902 <hakim@blocksphere.id>
2021-11-04 18:41:05 +0800

    MYR-1004: added new job

---
+++ src/jobs/update-exchange-rate.job.ts
@@ -0,0 +1,61 @@
+import {CronJob, cronJob} from '@loopback/cron';
+import {inject} from '@loopback/core';
+import {CoinMarketCap} from '../services';
+import {repository} from '@loopback/repository';
+import {CurrencyRepository, ExchangeRateRepository} from '../repositories';
+
+@cronJob()
+export class UpdateExchangeRateJob extends CronJob {
+  constructor(
+    @repository(CurrencyRepository)
+    protected currencyRepository: CurrencyRepository,
+    @repository(ExchangeRateRepository)
+    protected exchangeRateRepository: ExchangeRateRepository,
+    @inject('services.CoinMarketCap')
+    protected coinMarketCapService: CoinMarketCap,
+  ) {
+    super({
+      name: 'update-coin-market-cap-job',
+      onTick: async () => {
+        this.performJob();
+      },
+      cronTime: '0 */5 * * * *',
+      start: true,
+    });
+  }
+
+  async performJob() {
+    const currencies = await this.currencyRepository.find();
+    const currencyIds = currencies.map(currency => currency.id);
+
+    for (let i = 0; i < currencyIds.length; i++) {
+      const currencyId = currencyIds[i];
+
+      try {
+        const {data} = await this.coinMarketCapService.getActions(
+          `cryptocurrency/quotes/latest?symbol=${currencyId}`,
+        );
+        const price = data[currencyId].quote.USD.price;
+        const found = await this.exchangeRateRepository.findOne({
+          where: {
+            id: currencyId,
+          },
+        });
+
+        if (found) {
+          await this.exchangeRateRepository.updateById(currencyId, {
+            price: price,
+            updatedAt: new Date().toString(),
+          });
+        } else {
+          await this.exchangeRateRepository.create({
+            id: currencyId,
+            price: price,
+          });
+        }
+      } catch {
+        // ignore
+      }
+    }
+  }
+}
