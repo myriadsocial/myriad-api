ea13c7fe28be5d1a43806feadb2a57537d66f9db
abdulhakim2902 <hakim@blocksphere.id>
2021-11-08 15:38:03 +0800

    MYR-1162: added new setting controllers

---
+++ src/controllers/user-account-setting.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  get,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  param,
+  patch,
+  requestBody,
+} from '@loopback/rest';
+import {AccountSetting} from '../models';
+import {UserRepository} from '../repositories';
+
+export class UserAccountSettingController {
+  constructor(
+    @repository(UserRepository) protected userRepository: UserRepository,
+  ) {}
+
+  @get('/users/{id}/account-setting', {
+    responses: {
+      '200': {
+        description: 'User has one AccountSetting',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(AccountSetting),
+          },
+        },
+      },
+    },
+  })
+  async get(
+    @param.path.string('id') id: string,
+    @param.query.object('filter') filter?: Filter<AccountSetting>,
+  ): Promise<AccountSetting> {
+    return this.userRepository.accountSetting(id).get(filter);
+  }
+
+  @patch('/users/{id}/account-setting', {
+    responses: {
+      '200': {
+        description: 'User.AccountSetting PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async patch(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(AccountSetting, {partial: true}),
+        },
+      },
+    })
+    accountSetting: Partial<AccountSetting>,
+    @param.query.object('where', getWhereSchemaFor(AccountSetting))
+    where?: Where<AccountSetting>,
+  ): Promise<Count> {
+    return this.userRepository.accountSetting(id).patch(accountSetting, where);
+  }
+}
