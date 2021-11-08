2c3e7f59d15c74f53047269ca65748048aa2da60
abdulhakim2902 <hakim@blocksphere.id>
2021-11-08 15:26:15 +0800

    MYR-1162: added new enum type

---
+++ src/enums/account-setting-type.enum.ts
@@ -0,0 +1,4 @@
+export enum AccountSettingType {
+  PRIVATE = 'private',
+  PUBLIC = 'public',
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -10,3 +10,4 @@ export * from './visibility-type.enum';
 export * from './report-status-type.enum';
 export * from './reference-type.enum';
 export * from './report-type.enum';
+export * from './account-setting-type.enum';
