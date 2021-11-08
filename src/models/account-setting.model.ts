6732fd7053d386d0eefefc1e0f5271b681d994fd
abdulhakim2902 <hakim@blocksphere.id>
2021-11-08 15:27:07 +0800

    MYR-1162: added seting models

---
+++ src/models/account-setting.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {AccountSettingType} from '../enums';
+import {User} from './user.model';
+
+@model({
+  settings: {
+    strictObjectIDCoercion: true,
+    mongodb: {
+      collection: 'accountSettings',
+    },
+  },
+})
+export class AccountSetting extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+    mongodb: {
+      dataType: 'ObjectId',
+    },
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: false,
+    jsonSchema: {
+      enum: Object.values(AccountSettingType),
+    },
+    default: AccountSettingType.PUBLIC,
+  })
+  accountPrivacy: AccountSettingType;
+
+  @property({
+    type: 'string',
+    required: false,
+    jsonSchema: {
+      enum: Object.values(AccountSettingType),
+    },
+    default: AccountSettingType.PUBLIC,
+  })
+  socialMediaPrivacy: AccountSettingType;
+
+  @belongsTo(() => User)
+  userId: string;
+
+  constructor(data?: Partial<AccountSetting>) {
+    super(data);
+  }
+}
+
+export interface AccountSettingRelations {
+  // describe navigational properties here
+}
+
+export type AccountSettingWithRelations = AccountSetting &
+  AccountSettingRelations;
