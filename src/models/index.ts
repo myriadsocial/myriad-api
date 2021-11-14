08da43d585d540ebbb730d56ef5a6c943a820362
abdulhakim2902 <hakim@blocksphere.id>
2021-11-14 21:30:20 +0800

    MYR-1225: added new model

--- src/models/index.ts
+++ src/models/index.ts
@@ -29,3 +29,4 @@ export * from './report-detail.model';
 export * from './exchange-rate.model';
 export * from './account-setting.model';
 export * from './notification-setting.model';
+export * from './experience-user.model';
