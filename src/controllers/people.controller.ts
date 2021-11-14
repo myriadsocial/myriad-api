7d1ff2be5ab48b348815ba5f889d21fc7c7da09b
abdulhakim2902 <hakim@blocksphere.id>
2021-11-14 21:33:04 +0800

    MYR-1225: added new endpoint

--- src/controllers/people.controller.ts
+++ src/controllers/people.controller.ts
@@ -1,9 +1,10 @@
 import {intercept} from '@loopback/core';
 import {Filter, FilterExcludingWhere, repository} from '@loopback/repository';
 import {del, get, getModelSchemaRef, param, response} from '@loopback/rest';
+import {PlatformType} from '../enums';
 import {PaginationInterceptor} from '../interceptors';
 import {People} from '../models';
-import {PeopleRepository} from '../repositories';
+import {PeopleRepository, UserRepository} from '../repositories';
 // import {authenticate} from '@loopback/authentication';
 
 // @authenticate("jwt")
@@ -11,6 +12,8 @@ export class PeopleController {
   constructor(
     @repository(PeopleRepository)
     protected peopleRepository: PeopleRepository,
+    @repository(UserRepository)
+    protected userRepository: UserRepository,
   ) {}
 
   @intercept(PaginationInterceptor.BINDING_KEY)
@@ -31,6 +34,76 @@ export class PeopleController {
     filter?: Filter<People>,
   ): Promise<People[]> {
     return this.peopleRepository.find(filter);
+  }
+
+  @get('/people/search')
+  @response(200, {
+    description: 'Array of People model instance',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(People),
+        },
+      },
+    },
+  })
+  async searchPeople(@param.query.string('q') q?: string): Promise<People[]> {
+    if (!q) return [];
+    const pattern = new RegExp('^' + q, 'i');
+
+    const users = await this.userRepository.find({
+      where: {
+        or: [
+          {
+            name: {
+              regexp: pattern,
+            },
+          },
+          {
+            username: {
+              regexp: pattern,
+            },
+          },
+        ],
+      },
+      order: ['createdAt DESC'],
+      limit: 5,
+    });
+
+    const userToPeople = users.map(user => {
+      return new People({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        platform: PlatformType.MYRIAD,
+        originUserId: user.id,
+        profilePictureURL: user.profilePictureURL,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      });
+    });
+
+    const people = await this.peopleRepository.find({
+      where: {
+        or: [
+          {
+            name: {
+              regexp: pattern,
+            },
+          },
+          {
+            username: {
+              regexp: pattern,
+            },
+          },
+        ],
+      },
+      order: ['createdAt DESC'],
+      limit: 10 - userToPeople.length,
+    });
+
+    return [...userToPeople, ...people];
   }
 
   @get('/people/{id}')
