19a9e4b3d8973bf491fc9b3c5654a6ca47ea4774
abdulhakim2902 <hakim@blocksphere.id>
2021-12-01 00:02:44 +0800

    MYR-1305: reupdated migrate model and repository

--- src/models/migrate.model.ts
+++ src/models/migrate.model.ts
@@ -4,11 +4,11 @@ import {Entity, model, property} from '@loopback/repository';
   settings: {
     strictObjectIDCoercion: true,
     mongodb: {
-      collection: 'publicMetrics',
+      collection: 'postImporters',
     },
   },
 })
-export class PublicMetric extends Entity {
+export class PostImporter extends Entity {
   @property({
     type: 'string',
     id: true,
@@ -20,180 +20,8 @@ export class PublicMetric extends Entity {
   id?: string;
 }
 
-export interface PublicMetricRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'tokens',
-    },
-  },
-})
-export class Token extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
+export interface PostImporterRelations {
+  // describe navigational properties here
 }
 
-export interface TokenRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'userCredentials',
-    },
-  },
-})
-export class UserCredential extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface UserCredentialRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'userTokens',
-    },
-  },
-})
-export class UserToken extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface UserTokenRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'conversations',
-    },
-  },
-})
-export class Conversation extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface ConversationRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'detailTransactions',
-    },
-  },
-})
-export class DetailTransaction extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface DetailTransactionRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'tips',
-    },
-  },
-})
-export class Tip extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface TipRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'dislikes',
-    },
-  },
-})
-export class Dislike extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface DislikeRelations {}
-
-@model({
-  settings: {
-    strictObjectIDCoercion: true,
-    mongodb: {
-      collection: 'likes',
-    },
-  },
-})
-export class Like extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: true,
-    mongodb: {
-      dataType: 'ObjectId',
-    },
-  })
-  id?: string;
-}
-
-export interface LikeRelations {}
+export type PostImporterWithRelations = PostImporter & PostImporterRelations;
