5407821e605014d7aaf7fdb11ec6a4a4da37b68c
abdulhakim2902 <hakim@blocksphere.id>
2021-12-01 00:02:44 +0800

    MYR-1305: reupdated migrate model and repository

--- src/repositories/migrate.repository.ts
+++ src/repositories/migrate.repository.ts
@@ -1,113 +1,14 @@
 import {inject} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {
-  Conversation,
-  ConversationRelations,
-  DetailTransaction,
-  DetailTransactionRelations,
-  Dislike,
-  DislikeRelations,
-  Like,
-  LikeRelations,
-  PublicMetric,
-  PublicMetricRelations,
-  Tip,
-  TipRelations,
-  Token,
-  TokenRelations,
-  UserCredential,
-  UserCredentialRelations,
-  UserToken,
-  UserTokenRelations,
-} from '../models';
+import {PostImporter, PostImporterRelations} from '../models';
 
-export class PublicMetricRepository extends DefaultCrudRepository<
-  PublicMetric,
-  typeof PublicMetric.prototype.id,
-  PublicMetricRelations
+export class PostImporterRepository extends DefaultCrudRepository<
+  PostImporter,
+  typeof PostImporter.prototype.id,
+  PostImporterRelations
 > {
   constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(PublicMetric, dataSource);
-  }
-}
-
-export class TokenRepository extends DefaultCrudRepository<
-  Token,
-  typeof Token.prototype.id,
-  TokenRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(Token, dataSource);
-  }
-}
-
-export class UserCredentialRepository extends DefaultCrudRepository<
-  UserCredential,
-  typeof UserCredential.prototype.id,
-  UserCredentialRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(UserCredential, dataSource);
-  }
-}
-
-export class UserTokenRepository extends DefaultCrudRepository<
-  UserToken,
-  typeof UserToken.prototype.id,
-  UserTokenRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(UserToken, dataSource);
-  }
-}
-
-export class ConversationRepository extends DefaultCrudRepository<
-  Conversation,
-  typeof Conversation.prototype.id,
-  ConversationRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(Conversation, dataSource);
-  }
-}
-
-export class DetailTransactionRepository extends DefaultCrudRepository<
-  DetailTransaction,
-  typeof DetailTransaction.prototype.id,
-  DetailTransactionRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(DetailTransaction, dataSource);
-  }
-}
-
-export class TipRepository extends DefaultCrudRepository<
-  Tip,
-  typeof Tip.prototype.id,
-  TipRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(Tip, dataSource);
-  }
-}
-
-export class DislikeRepository extends DefaultCrudRepository<
-  Dislike,
-  typeof Dislike.prototype.id,
-  DislikeRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(Dislike, dataSource);
-  }
-}
-
-export class LikeRepository extends DefaultCrudRepository<
-  Like,
-  typeof Like.prototype.id,
-  LikeRelations
-> {
-  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
-    super(Like, dataSource);
+    super(PostImporter, dataSource);
   }
 }
