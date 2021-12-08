384d55dcddd3eb28da793035d45f4a672bd04ee7
abdulhakim2902 <hakim@blocksphere.id>
2021-12-08 22:40:11 +0700

    removed penalty status

--- src/enums/report-status-type.enum.ts
+++ src/enums/report-status-type.enum.ts
@@ -2,12 +2,4 @@ export enum ReportStatusType {
   PENDING = 'pending',
   REMOVED = 'removed',
   IGNORED = 'ignored',
-  APPROVED = 'approved',
-}
-
-export enum PenaltyStatusType {
-  BANNED = 'banned',
-  PENALTY1 = 'penalty 1',
-  PENALTY2 = 'penalty 2',
-  PENALTY3 = 'penalty 3',
 }
