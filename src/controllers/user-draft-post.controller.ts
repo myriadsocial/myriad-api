29f4f7e228a72e5542d4f91dcfbcfd065632a179
abdulhakim2902 <hakim@blocksphere.id>
2021-12-09 18:39:10 +0700

    added endpoint for handle draft post

---
+++ src/controllers/user-draft-post.controller.ts
@@ -0,0 +1,45 @@
+import {Count, CountSchema, repository} from '@loopback/repository';
+import {del, get, getModelSchemaRef, param, response} from '@loopback/rest';
+import {DraftPost} from '../models';
+import {DraftPostRepository} from '../repositories';
+// import {authenticate} from '@loopback/authentication';
+
+// @authenticate("jwt")
+export class UserDraftPostController {
+  constructor(
+    @repository(DraftPostRepository)
+    protected draftPostRepository: DraftPostRepository,
+  ) {}
+
+  @get('/users/{userId}/draft')
+  @response(200, {
+    description: 'Draft Post model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(DraftPost, {includeRelations: true}),
+      },
+    },
+  })
+  async getDraftPost(
+    @param.path.string('userId') userId: string,
+  ): Promise<DraftPost | null> {
+    return this.draftPostRepository.findOne({
+      where: {
+        createdBy: userId,
+      },
+    });
+  }
+
+  @del('/users/{userId}/draft')
+  @response(200, {
+    description: 'Delete Draft Post model instance',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async deleteDraftPost(
+    @param.path.string('userId') userId: string,
+  ): Promise<Count> {
+    return this.draftPostRepository.deleteAll({
+      createdBy: userId,
+    });
+  }
+}
