2abb831c80f1fbe7f2a0e9214052e64e7c81b428
abdulhakim2902 <hakim@blocksphere.id>
2021-12-15 12:48:32 +0700

    remove validation when updating usernam

--- src/services/activity-log.service.ts
+++ src/services/activity-log.service.ts
@@ -1,6 +1,5 @@
 import {BindingScope, injectable} from '@loopback/core';
 import {repository} from '@loopback/repository';
-import {HttpErrors} from '@loopback/rest';
 import {ActivityLogType, ReferenceType} from '../enums';
 import {ActivityLog} from '../models';
 import {ActivityLogRepository, LeaderBoardRepository} from '../repositories';
@@ -34,15 +33,8 @@ export class ActivityLogService {
       referenceType,
     });
 
-    if (count >= 1 && type === ActivityLogType.CREATEUSERNAME) {
-      throw new HttpErrors.UnprocessableEntity(
-        'You can only updated username once',
-      );
-    }
-
     await this.activityLogRepository.create(activityLog);
 
-    if (type === ActivityLogType.CREATEUSERNAME) return;
     if (count === 0) {
       // count activity
       const found = await this.leaderboardRepository.findOne({
