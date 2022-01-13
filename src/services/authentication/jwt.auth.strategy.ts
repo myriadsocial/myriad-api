7789fbf4a4b9f7c2bff6b1ed16c69562f4be7d31
abdulhakim2902 <hakim@blocksphere.id>
2022-01-13 18:52:48 +0700

    MYR-1481: fixed page meta data

--- src/services/authentication/jwt.auth.strategy.ts
+++ src/services/authentication/jwt.auth.strategy.ts
@@ -25,6 +25,7 @@ export class JWTAuthenticationStrategy implements AuthenticationStrategy {
         username: userProfile.username,
         createdAt: userProfile.createdAt,
       },
+      include: ['accountSetting', 'notificationSetting']
     });
 
     if (!user) throw new HttpErrors.Unauthorized('Forbidden user!');
