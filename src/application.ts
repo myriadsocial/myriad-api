3d9cc6d18bee1e293f93553cb9bcc4311dfd576f
Muhammad Abdul Hakim Shibghatallah <70675129+abdulhakim2902@users.noreply.github.com>
2022-01-19 15:01:30 +0700

    MYR-1481: created migration script (#373)

--- src/application.ts
+++ src/application.ts
@@ -2,7 +2,11 @@ import {AuthenticationComponent} from '@loopback/authentication';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig, createBindingFromClass} from '@loopback/core';
 import {HealthComponent} from '@loopback/health';
-import {RepositoryMixin} from '@loopback/repository';
+import {
+  AnyObject,
+  RepositoryMixin,
+  SchemaMigrationOptions,
+} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
@@ -48,12 +52,30 @@ import {v4 as uuid} from 'uuid';
 import {FILE_UPLOAD_SERVICE} from './keys';
 import {FCSService} from './services/fcs.service';
 import {
+  AccountSettingRepository,
+  ActivityLogRepository,
+  CommentRepository,
   CurrencyRepository,
+  DraftPostRepository,
   ExchangeRateRepository,
+  ExperienceRepository,
+  ExperienceUserRepository,
+  FriendRepository,
+  NotificationRepository,
+  NotificationSettingRepository,
   PeopleRepository,
+  PostRepository,
+  TransactionRepository,
+  UserCurrencyRepository,
+  UserExperienceRepository,
+  UserReportRepository,
+  UserRepository,
+  UserSocialMediaRepository,
+  VoteRepository,
 } from './repositories';
-import {PlatformType} from './enums';
-import {People} from './models';
+import {DefaultCurrencyType, NotificationType, PlatformType} from './enums';
+import {People, User} from './models';
+import {BcryptHasher} from './services/authentication/hash.password.service';
 
 export {ApplicationConfig};
 
@@ -150,6 +172,304 @@ export class MyriadApiApplication extends BootMixin(
       dsn: process.env.SENTRY_DSN,
       tracesSampleRate: 1.0,
     });
+  }
+
+  async migrateSchema(options?: SchemaMigrationOptions): Promise<void> {
+    await super.migrateSchema(options);
+
+    if (options?.existingSchema === 'drop') {
+      return this.doCreateIntialUser();
+    }
+
+    await this.doMigrateMyriadPublicKey();
+    await this.doRemoveCollection();
+    await this.doUpdatePeopleWallet();
+    await this.doChangeBaseStorageURL();
+  }
+
+  async doCreateIntialUser(): Promise<void> {
+    if (!this.options.user) return;
+    const {userRepository} = await this.repositories();
+
+    const user = await userRepository.create({
+      id: config.MYRIAD_OFFICIAL_ACCOUNT_PUBLIC_KEY,
+      name: 'Myriad Official',
+      username: 'myriad_official',
+      profilePictureURL:
+        'https://pbs.twimg.com/profile_images/1407599051579617281/-jHXi6y5_400x400.jpg',
+      bannerImageUrl:
+        'https://pbs.twimg.com/profile_banners/1358714439583690753/1624432887/1500x500',
+      bio: 'A social metaverse & metasocial network on web3, pulling content from mainstream social media and turning every post into a tipping wallet.',
+      websiteURL: 'https://www.myriad.social/',
+    });
+
+    await userRepository.accountSetting(user.id).create({});
+    await userRepository.notificationSetting(user.id).create({});
+    await userRepository.currencies(user.id).delete({
+      id: DefaultCurrencyType.MYRIA,
+    });
+    await userRepository.currencies(user.id).create({
+      id: DefaultCurrencyType.MYRIA,
+      decimal: 18,
+      image:
+        'https://pbs.twimg.com/profile_images/1407599051579617281/-jHXi6y5_400x400.jpg',
+      rpcURL: config.MYRIAD_RPC_WS_URL,
+      native: true,
+      networkType: 'substrate',
+      exchangeRate: false,
+    });
+
+    console.log('initial user have been successfully created');
+  }
+
+  async doMigrateMyriadPublicKey(): Promise<void> {
+    const userRepository = await this.getRepository(UserRepository);
+    const myriad = await userRepository.findOne({
+      where: {username: 'myriad_official'},
+    });
+
+    await this.updateMyriadIdInAllCollection(myriad);
+  }
+
+  async doRemoveCollection(): Promise<void> {
+    if (!this.options.remove) return;
+    const {notificationRepository} = await this.repositories();
+
+    await notificationRepository.deleteAll({
+      or: [
+        {type: NotificationType.COMMENT_TIPS},
+        {type: NotificationType.POST_TIPS},
+        {type: NotificationType.USER_REWARD},
+        {type: NotificationType.USER_INITIAL_TIPS},
+        {type: NotificationType.USER_CLAIM_TIPS},
+      ],
+    });
+
+    console.log(
+      'notification related to tipping have been successfully removed',
+    );
+  }
+
+  async doChangeBaseStorageURL(): Promise<void> {
+    if (!this.options.environment) return;
+    const environment = this.options.environment;
+
+    const {
+      userRepository,
+      currencyRepository,
+      experienceRepository,
+      postRepository,
+    } = await this.repositories();
+
+    const baseStorageURL: {[key: string]: string} = {
+      development:
+        'https://storage.googleapis.com/myriad-substrate-development.appspot.com',
+      staging:
+        'https://storage.googleapis.com/myriad-substrate-staging.appspot.com',
+      testnet:
+        'https://storage.googleapis.com/myriad-social-testnet.appspot.com',
+      mainnet:
+        'https://storage.googleapis.com/myriad-social-mainnet.appspot.com',
+    };
+    const newBaseStorageURL = baseStorageURL[environment];
+    const oldStorageURL: string[] = [];
+
+    for (const property in baseStorageURL) {
+      if (property !== environment) {
+        oldStorageURL.push(baseStorageURL[property]);
+      }
+    }
+    const regexp = new RegExp(oldStorageURL.join('|'), 'i');
+
+    // Replace base storage url in user collection
+    const {count: countUser} = await userRepository.count();
+
+    for (let i = 0; i < countUser; i++) {
+      const [user] = await userRepository.find({
+        limit: 1,
+        skip: i,
+      });
+
+      await userRepository.updateById(user.id, {
+        profilePictureURL: user.profilePictureURL?.replace(
+          regexp,
+          newBaseStorageURL,
+        ),
+        bannerImageUrl: user.bannerImageUrl?.replace(regexp, newBaseStorageURL),
+      });
+    }
+
+    console.log('user image have been successfully updated');
+
+    // Replace base storage url in currency collection
+    const {count: countCurrency} = await currencyRepository.count();
+
+    for (let i = 0; i < countCurrency; i++) {
+      const [currency] = await currencyRepository.find({
+        limit: 1,
+        skip: i,
+      });
+
+      await currencyRepository.updateById(currency.id, {
+        image: currency.image?.replace(regexp, newBaseStorageURL),
+      });
+    }
+
+    console.log('currency image have been successfully updated');
+
+    // Replace base storage url in experience collection
+    const {count: countExperience} = await experienceRepository.count();
+
+    for (let i = 0; i < countExperience; i++) {
+      const [experience] = await experienceRepository.find({
+        limit: 1,
+        skip: i,
+      });
+
+      await experienceRepository.updateById(experience.id, {
+        experienceImageURL: experience.experienceImageURL?.replace(
+          regexp,
+          newBaseStorageURL,
+        ),
+      });
+    }
+
+    console.log('experience image have been successfully updated');
+
+    // Replace base storage url in post collection
+    const {count: countPost} = await postRepository.count();
+
+    for (let i = 0; i < countPost; i++) {
+      const [post] = await postRepository.find({
+        limit: 1,
+        skip: i,
+      });
+
+      await postRepository.updateById(post.id, {
+        text: post.text?.replace(regexp, newBaseStorageURL),
+      });
+    }
+
+    console.log('post image have been successfully updated');
+  }
+
+  async doUpdatePeopleWallet(): Promise<void> {
+    if (!this.options.wallet) return;
+    const {peopleRepository} = await this.repositories();
+    const {count} = await peopleRepository.count();
+
+    const foundPeople = await peopleRepository.findOne();
+    const hasher = new BcryptHasher();
+
+    for (let i = 0; i < count; i++) {
+      const [people] = await peopleRepository.find({
+        limit: 1,
+        skip: i,
+      });
+
+      const hashPeopleId = await hasher.hashPassword(
+        people.id + config.MYRIAD_ESCROW_SECRET_KEY,
+      );
+
+      const match = await hasher.comparePassword(
+        hashPeopleId,
+        foundPeople.walletAddressPassword,
+      );
+
+      if (match) continue;
+
+      await peopleRepository.updateById(people.id, {
+        walletAddressPassword: hashPeopleId,
+      });
+    }
+
+    console.log('people wallet address have been successfully updated');
+  }
+
+  async updateMyriadIdInAllCollection(
+    myriadOfficial: User | null,
+  ): Promise<void> {
+    if (!myriadOfficial) return;
+    if (myriadOfficial.id === config.MYRIAD_OFFICIAL_ACCOUNT_PUBLIC_KEY) return;
+
+    const {
+      accountSettingRepository,
+      activityLogRepository,
+      commentRepository,
+      draftPostRepository,
+      experienceUserRepository,
+      experienceRepository,
+      friendRepository,
+      notificationRepository,
+      notificationSettingRepository,
+      postRepository,
+      transactionRepository,
+      userRepository,
+      userCurrencyRepository,
+      userExperienceRepository,
+      userReportRepository,
+      userSocMedRepository,
+      voteRepository,
+    } = await this.repositories();
+
+    const id = config.MYRIAD_OFFICIAL_ACCOUNT_PUBLIC_KEY;
+    const oldPublicKey = myriadOfficial.id;
+
+    await userRepository.deleteById(oldPublicKey);
+    await userRepository.create(Object.assign(myriadOfficial, {id}));
+
+    let data: AnyObject = {userId: id};
+    let where: AnyObject = {userId: oldPublicKey};
+    await accountSettingRepository.updateAll(data, where);
+
+    await activityLogRepository.updateAll(data, where);
+    await commentRepository.updateAll(data, where);
+    await experienceUserRepository.updateAll(data, where);
+    await notificationSettingRepository.updateAll(data, where);
+    await userCurrencyRepository.updateAll(data, where);
+    await userExperienceRepository.updateAll(data, where);
+    await userSocMedRepository.updateAll(data, where);
+    await voteRepository.updateAll(data, where);
+
+    data = {referenceId: id};
+    where = {referenceId: oldPublicKey};
+    await activityLogRepository.updateAll(data, where);
+    await notificationRepository.updateAll(data, where);
+    await transactionRepository.updateAll(data, where);
+
+    data = {createdBy: id};
+    where = {createdBy: oldPublicKey};
+    await draftPostRepository.updateAll(data, where);
+    await experienceRepository.updateAll(data, where);
+    await postRepository.updateAll(data, where);
+
+    data = {requesteeId: id};
+    where = {requesteeId: oldPublicKey};
+    await friendRepository.updateAll(data, where);
+
+    data = {requestorId: id};
+    where = {requestorId: oldPublicKey};
+    await friendRepository.updateAll(data, where);
+
+    data = {from: id};
+    where = {from: oldPublicKey};
+    await notificationRepository.updateAll(data, where);
+    await transactionRepository.updateAll(data, where);
+
+    data = {to: id};
+    where = {to: oldPublicKey};
+    await notificationRepository.updateAll(data, where);
+    await transactionRepository.updateAll(data, where);
+
+    data = {reportedBy: id};
+    where = {reportedBy: oldPublicKey};
+    await userReportRepository.updateAll(data, where);
+
+    data = {toUserId: id};
+    where = {toUserId: oldPublicKey};
+    await voteRepository.updateAll(data, where);
+
+    console.log('myriad public key have been successfully changed');
   }
 
   async initialPeopleProfile(): Promise<void> {
@@ -256,5 +576,67 @@ export class MyriadApiApplication extends BootMixin(
     } catch {
       // ignore
     }
+  }
+
+  async repositories(): Promise<AnyObject> {
+    const accountSettingRepository = await this.getRepository(
+      AccountSettingRepository,
+    );
+    const activityLogRepository = await this.getRepository(
+      ActivityLogRepository,
+    );
+    const commentRepository = await this.getRepository(CommentRepository);
+    const currencyRepository = await this.getRepository(CurrencyRepository);
+    const draftPostRepository = await this.getRepository(DraftPostRepository);
+    const experienceUserRepository = await this.getRepository(
+      ExperienceUserRepository,
+    );
+    const experienceRepository = await this.getRepository(ExperienceRepository);
+    const friendRepository = await this.getRepository(FriendRepository);
+    const notificationRepository = await this.getRepository(
+      NotificationRepository,
+    );
+    const notificationSettingRepository = await this.getRepository(
+      NotificationSettingRepository,
+    );
+    const peopleRepository = await this.getRepository(PeopleRepository);
+    const postRepository = await this.getRepository(PostRepository);
+    const transactionRepository = await this.getRepository(
+      TransactionRepository,
+    );
+    const userRepository = await this.getRepository(UserRepository);
+    const userCurrencyRepository = await this.getRepository(
+      UserCurrencyRepository,
+    );
+    const userExperienceRepository = await this.getRepository(
+      UserExperienceRepository,
+    );
+    const userReportRepository = await this.getRepository(UserReportRepository);
+    const userSocMedRepository = await this.getRepository(
+      UserSocialMediaRepository,
+    );
+    const voteRepository = await this.getRepository(VoteRepository);
+
+    return {
+      accountSettingRepository,
+      activityLogRepository,
+      commentRepository,
+      currencyRepository,
+      draftPostRepository,
+      experienceUserRepository,
+      experienceRepository,
+      friendRepository,
+      notificationRepository,
+      notificationSettingRepository,
+      peopleRepository,
+      postRepository,
+      transactionRepository,
+      userRepository,
+      userCurrencyRepository,
+      userExperienceRepository,
+      userReportRepository,
+      userSocMedRepository,
+      voteRepository,
+    };
   }
 }
