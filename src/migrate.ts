b3d357444e6d234ea56f8540c7884eee7efa53e4
abdulhakim2902 <hakim@blocksphere.id>
2022-01-19 12:45:43 +0700

    MYR-1481: created migration script

--- src/migrate.ts
+++ src/migrate.ts
@@ -2,9 +2,27 @@ import {MyriadApiApplication} from './application';
 
 export async function migrate(args: string[]) {
   const existingSchema = args.includes('--rebuild') ? 'drop' : 'alter';
+  const envIndex = args.indexOf('--environment');
+  const environment = args.includes('--environment')
+    ? args[envIndex + 1]
+      ? args[envIndex + 1]
+      : 'development'
+    : undefined;
+  const initialUser = args.includes('user');
+  const nonce = args.includes('nonce');
+  const post = args.includes('post');
+  const remove = args.includes('remove');
+  const wallet = args.includes('wallet');
   console.log('Migrating schemas (%s existing schema)', existingSchema);
 
-  const app = new MyriadApiApplication();
+  const app = new MyriadApiApplication({
+    environment,
+    user: initialUser,
+    nonce,
+    post,
+    remove,
+    wallet,
+  });
   await app.boot();
   await app.migrateSchema({existingSchema});
 
