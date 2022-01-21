39c1885fc02155c40de9b5c9d22c6f75feb82294
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2022-01-21 14:01:10 +0700

    MYR-1552: added airdrop functionality (#378)

--- src/utils/polkadotJs-utils.ts
+++ src/utils/polkadotJs-utils.ts
@@ -2,7 +2,7 @@ import {AnyObject} from '@loopback/repository';
 import {ApiPromise, Keyring, WsProvider} from '@polkadot/api';
 import {KeyringPair} from '@polkadot/keyring/types';
 import {u8aToHex} from '@polkadot/util';
-import {mnemonicGenerate} from '@polkadot/util-crypto';
+import {mnemonicGenerate, encodeAddress} from '@polkadot/util-crypto';
 import {KeypairType} from '@polkadot/util-crypto/types';
 
 export class PolkadotJs {
@@ -32,7 +32,11 @@ export class PolkadotJs {
   }
 
   async getSystemParameters(api: ApiPromise) {
+    const chainName = (await api.rpc.system.chain()).toHuman();
+    const chainType = (await api.rpc.system.chainType()).toHuman();
+    const genesisHash = (await api.query.system.blockHash(0)).toHuman();
     const params = await api.rpc.system.properties();
+    const ss58Format = params.ss58Format.value.toHuman();
     const decimals = params.tokenDecimals.value.toHuman() as string[];
     const symbols = params.tokenSymbol.value.toHuman() as string[];
     const symbolsDecimals = symbols.reduce(
@@ -44,10 +48,18 @@ export class PolkadotJs {
     );
 
     return {
+      chainName: chainName,
+      chainType: chainType,
+      genesisHash: genesisHash,
+      ss58Format: ss58Format,
       decimals: decimals as string[],
       symbols: symbols as string[],
       symbolsDecimals: symbolsDecimals as AnyObject,
     };
+  }
+
+  generateSeed(): string {
+    return mnemonicGenerate();
   }
 
   getKeyring(type = 'sr25519', addressFormat = 42): Keyring {
@@ -61,7 +73,11 @@ export class PolkadotJs {
     return u8aToHex(keyring.publicKey);
   }
 
-  generateSeed(): string {
-    return mnemonicGenerate();
+  publicKeyToString(publicKey: Uint8Array): string {
+    return u8aToHex(publicKey);
+  }
+
+  addressToPublicKey(address: string): string {
+    return encodeAddress(address);
   }
 }
