ac5159112b60be66041a0388ecc9f888ae603f05
abdulhakim2902 <hakim@blocksphere.id>
2022-03-08 13:17:45 +0700

    MYR-1808: added relation many to many experiencePost

--- src/models/experience.model.ts
+++ src/models/experience.model.ts
@@ -9,6 +9,8 @@ import {People} from './people.model';
 import {User} from './user.model';
 import {ExperienceUser} from './experience-user.model';
 import {UserWithRelations} from './user.model';
+import {Post} from './post.model';
+import {ExperiencePost} from './experience-post.model';
 
 @model({
   settings: {
@@ -132,6 +134,9 @@ export class Experience extends Entity {
 
   @hasMany(() => User, {through: {model: () => ExperienceUser}})
   users: User[];
+
+  @hasMany(() => Post, {through: {model: () => ExperiencePost}})
+  posts: Post[];
 
   constructor(data?: Partial<Experience>) {
     super(data);
--- src/repositories/experience.repository.ts
+++ src/repositories/experience.repository.ts
@@ -6,9 +6,18 @@ import {
   HasManyThroughRepositoryFactory,
 } from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {Experience, ExperienceRelations, User, ExperienceUser} from '../models';
+import {
+  Experience,
+  ExperienceRelations,
+  User,
+  ExperienceUser,
+  Post,
+  ExperiencePost,
+} from '../models';
 import {UserRepository} from './user.repository';
 import {ExperienceUserRepository} from './experience-user.repository';
+import {ExperiencePostRepository} from './experience-post.repository';
+import {PostRepository} from './post.repository';
 
 @bind({scope: BindingScope.SINGLETON})
 export class ExperienceRepository extends DefaultCrudRepository<
@@ -25,14 +34,31 @@ export class ExperienceRepository extends DefaultCrudRepository<
     typeof Experience.prototype.id
   >;
 
+  public readonly posts: HasManyThroughRepositoryFactory<
+    Post,
+    typeof Post.prototype.id,
+    ExperiencePost,
+    typeof Experience.prototype.id
+  >;
+
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
     @repository.getter('UserRepository')
     protected userRepositoryGetter: Getter<UserRepository>,
     @repository.getter('ExperienceUserRepository')
     protected experienceUserRepositoryGetter: Getter<ExperienceUserRepository>,
+    @repository.getter('ExperiencePostRepository')
+    protected experiencePostRepositoryGetter: Getter<ExperiencePostRepository>,
+    @repository.getter('PostRepository')
+    protected postRepositoryGetter: Getter<PostRepository>,
   ) {
     super(Experience, dataSource);
+    this.posts = this.createHasManyThroughRepositoryFactoryFor(
+      'posts',
+      postRepositoryGetter,
+      experiencePostRepositoryGetter,
+    );
+    this.registerInclusionResolver('posts', this.posts.inclusionResolver);
     this.users = this.createHasManyThroughRepositoryFactoryFor(
       'users',
       userRepositoryGetter,
