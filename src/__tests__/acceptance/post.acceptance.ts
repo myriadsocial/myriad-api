1cee0a03d430c73c98fbfc2e015cd05d953687bd
abdulhakim2902 <hakim@blocksphere.id>
2022-03-14 17:31:35 +0700

    MYR-1832: fixed testing

--- src/__tests__/acceptance/post.acceptance.ts
+++ src/__tests__/acceptance/post.acceptance.ts
@@ -365,6 +365,7 @@ describe('PostApplication', function () {
       banned: false,
       totalImporter: 1,
       popularCount: 0,
+      experienceIndex: {},
       user: toJSON(user),
       people: toJSON(people),
       comments: [toJSON(comment)],
