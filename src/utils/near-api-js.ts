a39c7da05b5ada4e39df1078a18289efe629b5f2
Muhammad Abdul Hakim Shibghatallah <70675129+abdulhakim2902@users.noreply.github.com>
2022-10-05 21:45:29 +0700

    fix: authentication near with alphanumeric (#647)

--- src/utils/near-api-js.ts
+++ src/utils/near-api-js.ts
@@ -1,7 +1,7 @@
 import {Credential} from '../models';
 import * as bs58 from 'bs58';
 import * as nearAPI from 'near-api-js';
-import {numberToHex, hexToU8a} from '@polkadot/util';
+import {numberToHex, hexToU8a, isHex} from '@polkadot/util';
 import nacl from 'tweetnacl';
 
 /* eslint-disable  @typescript-eslint/naming-convention */
@@ -17,9 +17,10 @@ export class Near {
     try {
       const pk = 'ed25519:' + bs58.encode(Buffer.from(hexToU8a(publicAddress)));
       const provider = new nearAPI.providers.JsonRpcProvider({url: rpcURL});
+      const address = isHex(accountId) ? accountId.substring(2) : accountId;
       const result = await provider.query({
         request_type: 'view_access_key',
-        account_id: accountId,
+        account_id: address,
         public_key: pk,
         finality: 'final',
       });
