3aeb94cd7ff3fcdac06465f617739ec8b31b6d34
rubenkristian <rubenkristian96@gmail.com>
2023-03-26 19:55:26 +0700

    feat: upload video file

---
+++ src/controllers/video-upload.controller.ts
@@ -0,0 +1,39 @@
+import {inject, service} from '@loopback/core';
+import {
+  post,
+  Request,
+  requestBody,
+  Response,
+  RestBindings,
+} from '@loopback/rest';
+import {VideoUploadService} from '../services/video-upload.service';
+
+export class VideoUploadController {
+  constructor(
+    @service(VideoUploadService)
+    private videoUploadService: VideoUploadService,
+  ) {}
+
+  @post('/upload/vide', {
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+            },
+          },
+        },
+        description: 'Files and fields',
+      },
+    },
+  })
+  async videoUpload(
+    @requestBody.file()
+    request: Request,
+    @inject(RestBindings.Http.RESPONSE)
+    response: Response,
+  ) {
+    return this.videoUploadService.uploadVideo(request, response);
+  }
+}
---
+++ src/services/video-upload.service.ts
@@ -0,0 +1,77 @@
+import {AuthenticationBindings} from '@loopback/authentication';
+import {inject} from '@loopback/core';
+import {HttpErrors, Request, Response} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {FILE_UPLOAD_SERVICE} from '../keys';
+import {FileUploadHandler} from '../types';
+import {upload, UploadType} from '../utils/upload';
+
+export class VideoUploadService {
+  private mimeTypes = [
+    'video/x-flv',
+    'video/mp4',
+    'application/x-mpegURL',
+    'video/MP2T',
+    'video/3gpp',
+    'video/quicktime',
+    'video/x-msvideo',
+    'video/x-ms-wmv',
+  ];
+  constructor(
+    @inject(FILE_UPLOAD_SERVICE)
+    private handler: FileUploadHandler,
+    @inject(AuthenticationBindings.CURRENT_USER, {optional: true})
+    private currentUser: UserProfile,
+  ) {}
+
+  async uploadVideo(request: Request, response: Response) {
+    const userId = this.currentUser[securityId];
+
+    if (!userId) {
+      throw new HttpErrors[400]();
+    }
+
+    return new Promise<object>((resolve, reject) => {
+      this.handler(request, response, err => {
+        if (err) reject(err);
+        else {
+          resolve(this.getFilesAndFields(userId, request));
+        }
+      });
+    });
+  }
+
+  async getFilesAndFields(userId: string, request: Request) {
+    const uploadedFiles = request.files;
+    const mapper = async (f: globalThis.Express.Multer.File) => {
+      if (!this.mimeTypes.includes(f.mimetype)) {
+        throw new HttpErrors[415]();
+      }
+
+      const targetDir = `/users/${userId}/video`;
+      const fileURL = await upload(UploadType.VIDEO, targetDir, f.path);
+
+      return {
+        fieldname: f.fieldname,
+        originalname: f.originalname,
+        mimetype: f.mimetype,
+        size: f.size,
+        url: fileURL,
+      };
+    };
+
+    let files: object[] = [];
+    if (Array.isArray(uploadedFiles)) {
+      files = await Promise.all(uploadedFiles.map(mapper));
+    } else {
+      for (const filename in uploadedFiles) {
+        files.push(...(await Promise.all(uploadedFiles[filename].map(mapper))));
+      }
+    }
+
+    return {
+      files,
+      fields: request.body,
+    };
+  }
+}
