d8e8657d2f02817a59f87190c86c273c3ffc28a4
RXRD <lilixinxin@protonmail.com>
2023-07-12 13:48:04 +0700

    Update post.controller.ts

--- src/controllers/user/post.controller.ts
+++ src/controllers/user/post.controller.ts
@@ -102,7 +102,7 @@ export class UserPostController {
           }
         })
       : undefined;
-    filter!.include = newinclusion ;
+    filter!.include = newinclusion;
     return this.userService.posts(filter);
   }
 
